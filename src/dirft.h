5d05c8669d288f6e61c7e0aa0176e418ac9b2802
ahbarnett <ahb@math.dartmouth.edu>
2017-01-26 12:45:29 -0500

    dir2d written

--- src/dirft.h
+++ src/dirft.h
@@ -7,4 +7,11 @@ void dirft1d1(BIGINT nj,double* x,dcomplex* c,int isign,BIGINT ms, dcomplex* f);
 void dirft1d2(BIGINT nj,double* x,dcomplex* c,int iflag,BIGINT ms, dcomplex* f);
 void dirft1d3(BIGINT nj,double* x,dcomplex* c,int iflag,BIGINT nk, double* s, dcomplex* f);
 
+void dirft2d1(BIGINT nj,double* x,double *y,dcomplex* c,int iflag,BIGINT ms, BIGINT mt, dcomplex* f);
+void dirft2d2(BIGINT nj,double* x,double *y,dcomplex* c,int iflag,BIGINT ms, BIGINT mt, dcomplex* f);
+void dirft2d3(BIGINT nj,double* x,double *y,dcomplex* c,int iflag,BIGINT nk, double* s, double* t, dcomplex* f);
+
+void dirft3d1(BIGINT nj,double* x,double *y,double *z,dcomplex* c,int iflag,BIGINT ms, BIGINT mt, BIGINT mu, dcomplex* f);
+void dirft3d2(BIGINT nj,double* x,double *y,double *z,dcomplex* c,int iflag,BIGINT ms, BIGINT mt, BIGINT mu, dcomplex* f);
+void dirft3d3(BIGINT nj,double* x,double *y,double *z,dcomplex* c,int iflag,BIGINT nk, double* s, double* t, double *u, dcomplex* f);
 #endif
--- src/dirft1d.cpp
+++ src/dirft1d.cpp
@@ -1,12 +1,12 @@
 #include "dirft.h"
 #include <iostream>
 
-// This is basically a port of dirftd1.f from CMCL package.
+// This is basically a port of dirft1d.f from CMCL package.
 
 void dirft1d1(BIGINT nj,double* x,dcomplex* c,int iflag,BIGINT ms, dcomplex* f)
 /* Direct computation of 1D type-1 nonuniform FFT. Interface same as finufft1d1.
 c                  1  nj-1
-c     f[k1]    =  --  SUM  c[j] exp(+/-i k1 x[j]) 
+c     f[k1]    =  --  SUM  c[j] exp(+-i k1 x[j]) 
 c                 nj  j=0
 c
 c     for -ms/2 <= k1 <= (ms-1)/2.
@@ -16,10 +16,10 @@ c     used, otherwise the - sign is used, in the exponential.
 */
 {
   BIGINT kmin = -(ms/2);                   // integer divide
-  for (BIGINT m=0;m<ms;++m) f[m] = 0.0;    // it knows f is complex type
+  for (BIGINT m=0;m<ms;++m) f[m] = {0,0};  // it knows f is complex type
   for (BIGINT j=0;j<nj;++j) {
     dcomplex a = (iflag>0) ? exp(ima*x[j]) : exp(-ima*x[j]);
-    dcomplex p = pow(a,kmin);
+    dcomplex p = pow(a,kmin);   // starting phase for most neg freq
     dcomplex cc = c[j]/(double)nj;   // 1/nj norm
     for (BIGINT m=0;m<ms;++m) {
       f[m] += cc * p;
@@ -31,7 +31,7 @@ c     used, otherwise the - sign is used, in the exponential.
 void dirft1d2(BIGINT nj,double* x,dcomplex* c,int iflag,BIGINT ms, dcomplex* f)
 /* Direct computation of 1D type-2 nonuniform FFT. Interface same as finufft1d2
 c
-c     c[j] = SUM   f[k1] exp(+/-i k1 x[j]) 
+c     c[j] = SUM   f[k1] exp(+-i k1 x[j]) 
 c             k1  
 c                            for j = 0,...,nj-1
 c     where sum is over -ms/2 <= k1 <= (ms-1)/2.
@@ -56,7 +56,7 @@ c     used, otherwise the - sign is used, in the exponential.
 void dirft1d3(BIGINT nj,double* x,dcomplex* c,int iflag,BIGINT nk, double* s, dcomplex* f)
 /* Direct computation of 1D type-3 nonuniform FFT. Interface same as finufft1d3
 c              nj-1
-c     f[k]  =  SUM   c[j] exp(+/-i s[k] x[j]) 
+c     f[k]  =  SUM   c[j] exp(+-i s[k] x[j]) 
 c              j=0                   
 c                    for k = 0, ..., nk-1
 c  If iflag>0 the + sign is used, otherwise the - sign is used, in the
