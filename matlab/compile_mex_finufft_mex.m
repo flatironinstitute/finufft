9529fafd77ca406434d14bde882541df595b9f26
Jeremy Magland <jeremy.magland@gmail.com>
2017-03-10 16:20:41 -0500

    change location of library

--- matlab/compile_mex_finufft_mex.m
+++ matlab/compile_mex_finufft_mex.m
@@ -1,3 +1,3 @@
-mex ./_mcwrap/mcwrap_finufft1d1_mex.cpp ./finufft_mex.cpp -output ./finufft1d1_mex ../src/finufft.a -lm -lgomp -lfftw3 -lfftw3_threads -lrt
-mex ./_mcwrap/mcwrap_finufft2d1_mex.cpp ./finufft_mex.cpp -output ./finufft2d1_mex ../src/finufft.a -lm -lgomp -lfftw3 -lfftw3_threads -lrt
-mex ./_mcwrap/mcwrap_finufft3d1_mex.cpp ./finufft_mex.cpp -output ./finufft3d1_mex ../src/finufft.a -lm -lgomp -lfftw3 -lfftw3_threads -lrt
+mex ./_mcwrap/mcwrap_finufft1d1_mex.cpp ./finufft_mex.cpp -output ./finufft1d1_mex ../lib/libfinufft.a -lm -lgomp -lfftw3 -lfftw3_threads -lrt
+mex ./_mcwrap/mcwrap_finufft2d1_mex.cpp ./finufft_mex.cpp -output ./finufft2d1_mex ../lib/libfinufft.a -lm -lgomp -lfftw3 -lfftw3_threads -lrt
+mex ./_mcwrap/mcwrap_finufft3d1_mex.cpp ./finufft_mex.cpp -output ./finufft3d1_mex ../lib/libfinufft.a -lm -lgomp -lfftw3 -lfftw3_threads -lrt
--- matlab/finufft_mex.h
+++ matlab/finufft_mex.h
@@ -9,7 +9,7 @@ MCWRAP [ COMPLEX uniform_data[N,1] ] = finufft1d1_mex( N, nonuniform_locations[M
     SET_INPUT M = size(nonuniform_locations,1)
     HEADERS finufft_mex.h
     SOURCES finufft_mex.cpp
-    MEXARGS ../src/finufft.a -lm -lgomp -lfftw3 -lfftw3_threads -lrt
+    MEXARGS ../lib/libfinufft.a -lm -lgomp -lfftw3 -lfftw3_threads -lrt
 
 */
 void finufft1d1_mex(int M, int N, double *uniform_data, double *nonuniform_locations, double *nonuniform_data, int isign, double tol, int num_threads);
@@ -19,7 +19,7 @@ MCWRAP [ COMPLEX uniform_data[N1,N2] ] = finufft2d1_mex( N1, N2, nonuniform_loca
     SET_INPUT M = size(nonuniform_locations,1)
     HEADERS finufft_mex.h
     SOURCES finufft_mex.cpp
-    MEXARGS ../src/finufft.a -lm -lgomp -lfftw3 -lfftw3_threads -lrt
+    MEXARGS ../lib/libfinufft.a -lm -lgomp -lfftw3 -lfftw3_threads -lrt
 
 */
 void finufft2d1_mex(int M, int N1, int N2, double *uniform_data, double *nonuniform_locations, double *nonuniform_data, int isign, double tol, int num_threads);
@@ -29,7 +29,7 @@ MCWRAP [ COMPLEX uniform_data[N1,N2,N3] ] = finufft3d1_mex( N1, N2, N3, nonunifo
     SET_INPUT M = size(nonuniform_locations,1)
     HEADERS finufft_mex.h
     SOURCES finufft_mex.cpp
-    MEXARGS ../src/finufft.a -lm -lgomp -lfftw3 -lfftw3_threads -lrt
+    MEXARGS ../lib/libfinufft.a -lm -lgomp -lfftw3 -lfftw3_threads -lrt
 
 */
 void finufft3d1_mex(int M, int N1, int N2, int N3, double *uniform_data, double *nonuniform_locations, double *nonuniform_data, int isign, double tol, int num_threads);
