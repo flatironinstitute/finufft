867b4c631e7c396075bd37886c8bb96d9f99af35
ahbarnett <ahb@math.dartmouth.edu>
2017-10-25 18:24:53 -0400

    modeord 1d tests in new demo1d1.py, checked in python & python3

---
+++ python_tests/demo1d1.py
@@ -0,0 +1,38 @@
+# convert DFM's simple demo to JFM interface, include modeord test.
+# Barnett 10/25/17
+
+import time
+import finufftpy
+import numpy as np
+
+np.random.seed(42)
+
+acc = 1.e-9
+iflag = 1
+N = int(1e6)
+M = int(1e5)
+x = np.random.uniform(-np.pi, np.pi, M)
+c = np.random.randn(M) + 1.j * np.random.randn(M)
+F = np.zeros([N], dtype=np.complex128)       # allocate F (modes out)
+
+strt = time.time()
+status = finufftpy.finufft1d1(x, c, iflag, acc, N, F)
+print("Finished nufft in {0:.2g} seconds. Checking..."
+      .format(time.time()-strt))
+
+n = 142519      # mode to check
+Ftest = 0.0
+# this is so slow...
+for j in range(M):
+    Ftest += c[j] * np.exp(n * x[j] * 1.j)
+Fmax = np.max(np.abs(F))
+err = np.abs((F[n + N // 2] - Ftest) / Fmax)
+print("Error relative to max of F: {0:.2e}".format(err))
+
+# now test FFT mode output version, overwriting F...
+strt = time.time()
+status = finufftpy.finufft1d1(x, c, iflag, acc, N, F, modeord=1)
+print("Finished nufft in {0:.2g} seconds (modeord=1)"
+      .format(time.time()-strt))
+err = np.abs((F[n] - Ftest) / Fmax)   # now zero offset in F array
+print("Error relative to max of F: {0:.2e}".format(err))
