1a26370063ebf638641d6f86288b16dba103b9d3
ahbarnett <ahb@math.dartmouth.edu>
2018-02-01 18:48:04 -0500

    finufft.h dulpicate R removed

--- src/finufft.h
+++ src/finufft.h
@@ -4,7 +4,6 @@
 #include "utils.h"
 
 struct nufft_opts {  // see common/finufft_default_opts() for defaults
-  FLT R;              // kernel-dep upsampling ratio (for experts)
   int debug;          // 0: silent, 1: text basic timing output
   int spread_debug;   // passed to spread_opts debug: 0,1 or 2
   int spread_sort;    // passed to spread_opts sort: 0 or 1
