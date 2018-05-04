84d1db5332f608d90b3dd3f0484d77ba72955b16
ahbarnett <ahb@math.dartmouth.edu>
2018-05-04 12:13:29 -0400

    matlab opts handling more robust, following joakim

--- matlab/finufft_opts.m
+++ matlab/finufft_opts.m
@@ -1,6 +1,7 @@
 function opts = finufft_opts(o)
 % FINUFFTS_OPTS.  Global set opts for matlab interface to FINUFFT, with defaults
 
+% defaults, used if field absent or empty...
 debug=0; if isfield(o,'debug') && ~isempty(o.debug), debug = o.debug; end
 nthreads=0; if isfield(o,'nthreads') && ~isempty(o.nthreads), nthreads = o.nthreads; end
 spread_sort=2; if isfield(o,'spread_sort') && ~isempty(o.spread_sort), spread_sort=o.spread_sort; end
@@ -9,4 +10,11 @@ modeord=0; if isfield(o,'modeord') && ~isempty(o.modeord), modeord=o.modeord; en
 chkbnds=1; if isfield(o,'chkbnds') && ~isempty(o.chkbnds), chkbnds=o.chkbnds; end
 
 % pack up: ordering of opts must match that in finufft_m.cpp:finufft_mex_opts()
-opts = double([debug,nthreads,spread_sort,fftw,modeord,chkbnds]);
+% (placement in opts now explicit, catches errors if inputs are not 1x1 sized)
+opts = zeros(1,6,'double');
+opts(1) = debug;
+opts(2) = nthreads;
+opts(3) = spread_sort;
+opts(4) = fftw;
+opts(5) = modeord;
+opts(6) = chkbnds;
