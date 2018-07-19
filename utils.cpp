a48015187e1941e5e1ef4350abd206f6b5d9e0b7
Yu-hsuan Shih <yoyoshih13@gmail.com>
2018-07-19 10:24:14 -0400

    using the same utils function in finufft

--- utils.cpp
+++ utils.cpp
@@ -1,4 +1,105 @@
 #include "utils.h"
+
+// ------------ complex array utils
+
+FLT relerrtwonorm(BIGINT n, CPX* a, CPX* b)
+// ||a-b||_2 / ||a||_2
+{
+  FLT err = 0.0, nrm = 0.0;
+  for (BIGINT m=0; m<n; ++m) {
+    nrm += real(conj(a[m])*a[m]);
+    CPX diff = a[m]-b[m];
+    err += real(conj(diff)*diff);
+  }
+  return sqrt(err/nrm);
+}
+FLT errtwonorm(BIGINT n, CPX* a, CPX* b)
+// ||a-b||_2
+{
+  FLT err = 0.0;   // compute error 2-norm
+  for (BIGINT m=0; m<n; ++m) {
+    CPX diff = a[m]-b[m];
+    err += real(conj(diff)*diff);
+  }
+  return sqrt(err);
+}
+FLT twonorm(BIGINT n, CPX* a)
+// ||a||_2
+{
+  FLT nrm = 0.0;
+  for (BIGINT m=0; m<n; ++m)
+    nrm += real(conj(a[m])*a[m]);
+  return sqrt(nrm);
+}
+FLT infnorm(BIGINT n, CPX* a)
+// ||a||_infty
+{
+  FLT nrm = 0.0;
+  for (BIGINT m=0; m<n; ++m) {
+    FLT aa = real(conj(a[m])*a[m]);
+    if (aa>nrm) nrm = aa;
+  }
+  return sqrt(nrm);
+}
+
+void arrayrange(BIGINT n, FLT* a, FLT *lo, FLT *hi)
+// With a a length-n array, writes out min(a) to lo and max(a) to hi,
+// so that all a values lie in [lo,hi].
+{
+  *lo = INFINITY; *hi = -INFINITY;
+  for (BIGINT m=0; m<n; ++m) {
+    if (a[m]<*lo) *lo = a[m];
+    if (a[m]>*hi) *hi = a[m];
+  }
+}
+
+void indexedarrayrange(BIGINT n, BIGINT* i, FLT* a, FLT *lo, FLT *hi)
+// With i a list of n indices, and a an array of length max(i), writes out
+// min(a(i)) to lo and max(a(i)) to hi, so that all a(i) values lie in [lo,hi].
+{
+  *lo = INFINITY; *hi = -INFINITY;
+  for (BIGINT m=0; m<n; ++m) {
+    FLT A=a[i[m]];
+    if (A<*lo) *lo = A;
+    if (A>*hi) *hi = A;
+  }
+}
+
+void arraywidcen(BIGINT n, FLT* a, FLT *w, FLT *c)
+// Writes out w = half-width and c = center of an interval enclosing all a[n]'s
+// Only chooses a nonzero center if this increases w by less than fraction
+// ARRAYWIDCEN_GROWFRAC defined in utils.h.
+// This prevents rephasings which don't grow nf by much. 6/8/17
+{
+  FLT lo,hi;
+  arrayrange(n,a,&lo,&hi);
+  *w = (hi-lo)/2;
+  *c = (hi+lo)/2;
+  if (FABS(*c)<ARRAYWIDCEN_GROWFRAC*(*w)) {
+    *w += FABS(*c);
+    *c = 0.0;
+  }
+}
+
+BIGINT next235even(BIGINT n)
+// finds even integer not less than n, with prime factors no larger than 5
+// (ie, "smooth"). Adapted from fortran in hellskitchen.  Barnett 2/9/17
+// changed INT64 type 3/28/17. Runtime is around n*1e-11 sec for big n.
+{
+  if (n<=2) return 2;
+  if (n%2 == 1) n+=1;   // even
+  BIGINT nplus = n-2;   // to cancel out the +=2 at start of loop
+  BIGINT numdiv = 2;    // a dummy that is >1
+  while (numdiv>1) {
+    nplus += 2;         // stays even
+    numdiv = nplus;
+    while (numdiv%2 == 0) numdiv /= 2;  // remove all factors of 2,3,5...
+    while (numdiv%3 == 0) numdiv /= 3;
+    while (numdiv%5 == 0) numdiv /= 5;
+  }
+  return nplus;
+}
+
 // ----------------------- helpers for timing (always stay double prec)...
 using namespace std;
 
@@ -8,7 +109,7 @@ void CNTime::start()
 }
 
 double CNTime::restart()
-  // Barnett changed to returning in sec
+// Barnett changed to returning in sec
 {
   double delta = this->elapsedsec();
   this->start();
@@ -16,23 +117,11 @@ double CNTime::restart()
 }
 
 double CNTime::elapsedsec()
-        // returns answers as double, in seconds, to microsec accuracy. Barnett 5/22/18
+// returns answers as double, in seconds, to microsec accuracy. Barnett 5/22/18
 {
   struct timeval now;
   gettimeofday(&now, 0);
   double nowsec = (double)now.tv_sec + 1e-6*now.tv_usec;
   double initialsec = (double)initial.tv_sec + 1e-6*initial.tv_usec;
   return nowsec - initialsec;
-}
-
-FLT relerrtwonorm(int n, CPX* a, CPX* b)
-//||a-b||_2 / ||a||_2
-{
-  FLT err = 0.0, nrm = 0.0;
-  for (int m=0; m<n; ++m) {
-      nrm += real(conj(a[m])*a[m]);
-      CPX diff = a[m]-b[m];
-      err += real(conj(diff)*diff);
-  }
-  return sqrt(err)/nrm;
 }
--- utils.h
+++ utils.h
@@ -1,37 +1,151 @@
-#ifndef __UTILS_H__
-#define __UTILS_H__
+// This contains some library-wide definitions & precision/OMP switches,
+// as well as the interfaces to utilities in utils.cpp. Barnett 6/18/18.
 
-#include <complex>
+#ifndef UTILS_H
+#define UTILS_H
+
+// octave (mkoctfile) needs this otherwise it doesn't know what int64_t is!
+#include <stdint.h>
+
+#include <complex>          // C++ type complex
+#include <fftw3.h>          // needed so can typedef FFTW_CPX
 #include <cuComplex.h>
 
-using namespace std;
+// fraction growth cut-off in arraywidcen(), to decide if translate in type-3
+#define ARRAYWIDCEN_GROWFRAC 0.1
 
-typedef double FLT;
-typedef complex<double> CPX;
-typedef cuDoubleComplex gpuComplex;
-
-#define rand01() ((FLT)rand()/RAND_MAX)
-// unif[-1,1]:
-#define IMA complex<FLT>(0.0,1.0)
-#define randm11() (2*rand01() - (FLT)1.0)
-#define crandm11() (randm11() + IMA*randm11())
-#define PI (FLT)M_PI
+// math consts not in math.h ...
 #define M_1_2PI 0.159154943091895336
-#define RESCALE(x,N,p) (p ? \
-             ((x*M_1_2PI + (x<-PI ? 1.5 : (x>PI ? -0.5 : 0.5)))*N) : \
-             (x<0 ? x+N : (x>N ? x-N : x)))
+#define M_2PI   6.28318530717958648
+// to avoid mixed precision operators in eg i*pi...
+#define PI (FLT)M_PI
+
+using namespace std;        // means std:: not needed for cout, max, etc
+
+typedef complex<double> dcomplex;  // slightly sneaky since duplicated by mwrap
+
+// Compile-flag choice of single or double (default) precision:
+// (Note in the other codes, FLT is "double" or "float", CPX same but complex)
+#ifdef SINGLE
+  // machine epsilon for rounding
+  #define EPSILON (float)6e-08
+  typedef float FLT;
+  typedef complex<float> CPX;
+  #define IMA complex<float>(0.0,1.0)
+  #define FABS(x) fabs(x)
+  typedef fftwf_complex FFTW_CPX;           //  single-prec has fftwf_*
+  typedef fftwf_plan FFTW_PLAN;
+  #define FFTW_INIT fftwf_init_threads
+  #define FFTW_PLAN_TH fftwf_plan_with_nthreads
+  #define FFTW_ALLOC_RE fftwf_alloc_real
+  #define FFTW_ALLOC_CPX fftwf_alloc_complex
+  #define FFTW_PLAN_1D fftwf_plan_dft_1d
+  #define FFTW_PLAN_2D fftwf_plan_dft_2d
+  #define FFTW_PLAN_3D fftwf_plan_dft_3d
+  #define FFTW_EX fftwf_execute
+  #define FFTW_DE fftwf_destroy_plan
+  #define FFTW_FR fftwf_free
+  #define gpuComplex cuFLoatComplex
+#else
+  // machine epsilon for rounding
+  #define EPSILON (double)1.1e-16
+  typedef double FLT;
+  typedef complex<double> CPX;
+  #define IMA complex<double>(0.0,1.0)
+  #define FABS(x) fabsf(x)
+  typedef fftw_complex FFTW_CPX;           // double-prec has fftw_*
+  typedef fftw_plan FFTW_PLAN;
+  #define FFTW_INIT fftw_init_threads
+  #define FFTW_PLAN_TH fftw_plan_with_nthreads
+  #define FFTW_ALLOC_RE fftw_alloc_real
+  #define FFTW_ALLOC_CPX fftw_alloc_complex
+  #define FFTW_PLAN_1D fftw_plan_dft_1d
+  #define FFTW_PLAN_2D fftw_plan_dft_2d
+  #define FFTW_PLAN_3D fftw_plan_dft_3d
+  #define FFTW_EX fftw_execute
+  #define FFTW_DE fftw_destroy_plan
+  #define FFTW_FR fftw_free
+  #define gpuComplex cuDoubleComplex
+#endif
+
+// All indexing in library that potentially can exceed 2^31 uses 64-bit signed.
+// This includes all calling arguments (eg M,N) that could be huge someday...
+typedef int64_t BIGINT;
+
+// Global error codes for the library...
+#define ERR_EPS_TOO_SMALL        1
+#define ERR_MAXNALLOC            2
+#define ERR_SPREAD_BOX_SMALL     3
+#define ERR_SPREAD_PTS_OUT_RANGE 4
+#define ERR_SPREAD_ALLOC         5
+#define ERR_SPREAD_DIR           6
+#define ERR_UPSAMPFAC_TOO_SMALL  7
+#define HORNER_WRONG_BETA        8
+#define ERR_NDATA_NOTVALID       9
+
+
+//#define MAX(a,b) (a>b) ? a : b  // but we use std::max instead
+#define MIN(a,b) (a<b) ? a : b
 #define max_shared_mem 6000
+
+// ahb math helpers
+FLT relerrtwonorm(BIGINT n, CPX* a, CPX* b);
+FLT errtwonorm(BIGINT n, CPX* a, CPX* b);
+FLT twonorm(BIGINT n, CPX* a);
+FLT infnorm(BIGINT n, CPX* a);
+void arrayrange(BIGINT n, FLT* a, FLT *lo, FLT *hi);
+void indexedarrayrange(BIGINT n, BIGINT* i, FLT* a, FLT *lo, FLT *hi);
+void arraywidcen(BIGINT n, FLT* a, FLT *w, FLT *c);
+BIGINT next235even(BIGINT n);
+
 
 // jfm timer class
 #include <sys/time.h>
 class CNTime {
-  public:
-    void start();
-    double restart();
-    double elapsedsec();
-  private:
-    struct timeval initial;
+ public:
+  void start();
+  double restart();
+  double elapsedsec();
+ private:
+  struct timeval initial;
 };
+
+
+// Random numbers: crappy unif random number generator in [0,1):
+//#define rand01() (((FLT)(rand()%RAND_MAX))/RAND_MAX)
+#define rand01() ((FLT)rand()/RAND_MAX)
+// unif[-1,1]:
+#define randm11() (2*rand01() - (FLT)1.0)
+// complex unif[-1,1] for Re and Im:
+#define crandm11() (randm11() + IMA*randm11())
+
+// Thread-safe seed-carrying versions of above (x is ptr to seed)...
+#define rand01r(x) ((FLT)rand_r(x)/RAND_MAX)
+// unif[-1,1]:
+#define randm11r(x) (2*rand01r(x) - (FLT)1.0)
+// complex unif[-1,1] for Re and Im:
+#define crandm11r(x) (randm11r(x) + IMA*randm11r(x))
+
+
+// allow compile-time switch off of openmp, so compilation without any openmp
+// is done (Note: _OPENMP is automatically set by -fopenmp compile flag)
+#ifdef _OPENMP
+  #include <omp.h>
+  // point to actual omp utils
+  #define MY_OMP_GET_NUM_THREADS() omp_get_num_threads()
+  #define MY_OMP_GET_MAX_THREADS() omp_get_max_threads()
+  #define MY_OMP_GET_THREAD_NUM() omp_get_thread_num()
+  #define MY_OMP_SET_NUM_THREADS(x) omp_set_num_threads(x)
+#else
+  // non-omp safe dummy versions of omp utils, and dummy fftw threads calls...
+  #define MY_OMP_GET_NUM_THREADS() 1
+  #define MY_OMP_GET_MAX_THREADS() 1
+  #define MY_OMP_GET_THREAD_NUM() 0
+  #define MY_OMP_SET_NUM_THREADS(x)
+  #undef FFTW_INIT
+  #define FFTW_INIT()
+  #undef FFTW_PLAN_TH
+  #define FFTW_PLAN_TH(x)
 #endif
 
-FLT relerrtwonorm(int n, CPX* a, CPX* b);
+#endif  // UTILS_H
