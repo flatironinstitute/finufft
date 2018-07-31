6446543ca2e16c111c4bf7e301e5210cfced7cc2
ahbarnett <ahb@math.dartmouth.edu>
2018-07-31 15:29:52 -0400

    ndata smaller in check2d.sh

--- test/check2d.sh
+++ test/check2d.sh
@@ -2,4 +2,4 @@
 # Standard checker for all 2d routines. Sed removes the timing lines (w/ "NU")
 ./finufft2d_test 1e2 1e1 1e3 $FINUFFT_REQ_TOL 0 | sed '/NU/d'
 # Melody's addition of the "many" interface test...
-./finufft2dmany_test 100 1e2 1e1 1e3 $FINUFFT_REQ_TOL 0 2 | sed '/NU/d;/T_/d' 
+./finufft2dmany_test 10 1e2 1e1 1e3 $FINUFFT_REQ_TOL 0 2 | sed '/NU/d;/T_/d' 
