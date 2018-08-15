c1efd9f40ae914041e9b478813fc6362693678db
Yu-hsuan Shih <yoyoshih13@gmail.com>
2018-08-15 15:37:48 -0400

    update/remove timing codes

---
+++ benchmark/script/interpkernel_cufinufft.py
@@ -0,0 +1,76 @@
+import subprocess
+import numpy as np
+import re
+
+def find_between( s, first, last ):
+    try:
+        start = s.index( first ) + len( first )
+        end = s.index( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
+
+def main():
+	nupts_distr=1
+	reps=5
+	density_totry = 10.0**np.arange(-1,2) #0.1, 1, 10
+	#density_totry = 10.0**np.arange(0,1) #0.1, 1, 10
+	tol_totry     = 10.0**np.linspace(-14,-2,4) #1e-14, 1e-10, 1e-6 , 1e-2
+	nf1_totry     = 2**np.arange(7,13) #128, ... ,4096 
+	t_gpuspread_5 = np.zeros([len(density_totry), len(tol_totry), len(nf1_totry)])
+	f= open("../results/cufinufft_interp_d_1_m1_0812.out","w")
+	f.write("cufinufft interp: method(1) idriven\n")
+	f.write('(density,tol,nf1,M)\tTime(HtoD(ms) + Spread(ms) + DtoH(ms))\n')
+	for d,density in enumerate(density_totry):
+		for t,tol in enumerate(tol_totry):
+			for n,nf1 in enumerate(nf1_totry):
+				M = int((nf1/2.0)*(nf1/2.0)*density)
+				# Method 1
+				"""
+				t = 0
+				for n in range(reps):
+					output=subprocess.check_output(["./compare",'1',str(nupts_distr),str(N),str(N)], \
+							    cwd="../../").decode("utf-8")
+					t+= float(find_between(output, "HtoD", "ms"))
+					t+= float(find_between(output, "Spread", "ms"))
+					t+= float(find_between(output, "DtoH", "ms"))
+				t_gpuspread_1[i] = t/reps
+				"""
+				# Method 5
+				print(d,t,n)
+				tnow = float('Inf')
+				for nn in range(reps):
+					tt = 0.0
+					output=subprocess.check_output(["./interp2d",'1',str(nupts_distr),str(nf1),str(nf1),str(M), \
+									 str(tol)], cwd="../../").decode("utf-8")
+					tt+= float(find_between(output, "HtoD", "ms"))
+					tt+= float(find_between(output, "Interp (1)", "ms"))
+					tt+= float(find_between(output, "DtoH", "ms"))
+					tnow = min(tnow,tt)
+				t_gpuspread_5[d,t,n] = tnow
+				f.write('({:5.1e},{:5.1e},{:5d},{:15d})\t t={:5.3e}\n'.format(density,tol,nf1,M,t_gpuspread_5[d,t,n]))
+	
+	np.save('../results/cufinufft_interp_d_1_m1_0812.npy', t_gpuspread_5)
+
+	# Output result
+	"""
+	print("Method 1: input driven without sort")
+	for i,N in enumerate(N_totry):
+		print('N={:5d}, t= {:5.3e}'.format(N,t_gpuspread_1[i]))
+	print("\n")
+
+	print("Method 2: input driven with sort")
+	for i,N in enumerate(N_totry):
+		print('N={:5d}, t= {:5.3e}'.format(N,t_gpuspread_2[i]))
+	print("\n")
+
+	print("Method 4: hybrid")
+	for i,N in enumerate(N_totry):
+		print('N={:5d}, t= {:5.3e}'.format(N,t_gpuspread_4[i]))
+	print("\n")
+	print("Method 5: subprob")
+	for i,N in enumerate(N_totry):
+		print('N={:5d}, t= {:5.3e}'.format(N,t_gpuspread_5[i]))
+  	"""
+if __name__== "__main__":
+  main()
--- benchmark/script/spreadkernel_cunfft.py
+++ benchmark/script/spreadkernel_cunfft.py
@@ -13,20 +13,20 @@ def find_between( s, first, last ):
 def main():
 	nupts_distr=2
 	reps=5
-	#density_totry = 10.0**np.arange(-1,2) #0.01, 0.1, 1, 10, 100
-	density_totry = 10.0**np.arange(0,1) #0.01, 0.1, 1, 10, 100
+	density_totry = 10.0**np.arange(-1,2) #0.01, 0.1, 1, 10, 100
+	#density_totry = 10.0**np.arange(0,1) #0.01, 0.1, 1, 10, 100
 	tol_totry     = 10.0**np.linspace(-14,-2,4) #1e-14, 1e-10, 1e-6 , 1e-2
 	nf1_totry     = 2**np.arange(7,13) #128, ... ,4096 
 	t_gpuspread_5 = np.zeros([len(density_totry), len(tol_totry), len(nf1_totry)])
-	f= open("../results/cunfft_spread_s_2_0812.out","w")
-	f.write("cunfft spread (clustered nupts):\n")
+	f= open("../results/cunfft_interp_d_2_0812.out","w")
+	f.write("cunfft interp (clustered nupts):\n")
 	f.write('(density,tol,nf1,M)\tTime(HtoD(ms) + Spread(ms) + DtoH(ms))\n')
 	for t,tol in enumerate(tol_totry):
 		cutoff=int((-np.log10(tol/10.0))/2.0)
 		print("cutoff={:5f}".format(int(cutoff)))
 		subprocess.call(["ls"],cwd="../../../CUNFFT/build/")
 		subprocess.call(["make", "distclean"],cwd="../../../CUNFFT/build/")
-		subprocess.call(["cmake", "..",  "-DCUT_OFF="+str(cutoff), "-DCUNFFT_DOUBLE_PRECISION=OFF", "-DMILLI_SEC=ON", "-DMEASURED_TIMES=ON", \
+		subprocess.call(["cmake", "..",  "-DCUT_OFF="+str(cutoff), "-DCUNFFT_DOUBLE_PRECISION=ON", "-DMILLI_SEC=ON", "-DMEASURED_TIMES=ON", \
                                          "-DCUDA_CUDA_LIBRARY=/usr/lib64/nvidia/libcuda.so", "-DPRINT_CONFIG=OFF"],cwd="../../../CUNFFT/build/")
 		subprocess.call(["make"],cwd="../../../CUNFFT/build/") #> makeLog.txt 2>&1
 		subprocess.call(["make", "install"], cwd="../../../CUNFFT/build/")
@@ -40,12 +40,12 @@ def main():
 					tt = 0.0
 					output=subprocess.check_output(["./cunfft_timing",str(nupts_distr),str(nf1),str(nf1),str(M)], cwd="../").decode("utf-8")
 					tt+= float(find_between(output, "HtoD", "ms"))
-					tt+= float(find_between(output, "spread", "ms"))
+					tt+= float(find_between(output, "interp", "ms"))
 					tt+= float(find_between(output, "DtoH", "ms"))
 					tnow = min(tnow,tt)
 				t_gpuspread_5[d,t,n] = tnow
 				f.write('({:5.1e},{:5.1e},{:5d},{:15d})\t t={:5.3e}\n'.format(density,tol,nf1,M,t_gpuspread_5[d,t,n]))
-	np.save('../results/cunfft_spread_s_2_0812.npy', t_gpuspread_5)
+	np.save('../results/cunfft_interp_d_2_0812.npy', t_gpuspread_5)
 
 if __name__== "__main__":
   main()
