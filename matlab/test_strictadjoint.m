c40695f8e9959fc2505dddef5768e3e84acae092
ahbarnett <ahb@math.dartmouth.edu>
2018-08-27 11:57:44 -0400

    added Fessler's suggested strict adjointness test in 1D, matlab

---
+++ matlab/test_strictadjoint.m
@@ -0,0 +1,15 @@
+% check t1 and t2 are adjoints to rounding error, not merely to requested tol.
+% 1d only for now. Barnett 8/27/18
+clear; addpath ~/numerics/finufft/matlab
+
+M=1e5;  % pts
+N=1e4;  % modes
+tol = 1e-6;
+x = pi*(2*rand(1,M)-1);
+% pick two vectors to check (u,F1 v) = (F2 u,v) with...
+v = randn(1,M)+1i*randn(1,M);
+u = randn(1,N)+1i*randn(1,N);
+ip1 = dot(u,finufft1d1(x,v,+1,tol,N));
+ip2 = dot(finufft1d2(x,-1,tol,u),v);    % note sign flips to be complex adjoint
+fprintf('M=%d,N=%d,tol=%.1g: rel err (u,F1 v) vs (F2 u,v): %.3g\n',M,N,tol,abs(ip1-ip2)/abs(ip1))
+fprintf('cf estimated rounding err for this prob size; %.3g\n',0.2*eps*N)
