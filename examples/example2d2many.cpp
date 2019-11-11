60f9118cf41caf10b2f4fedbb1b23fc35594db74
Yu-hsuan Shih <yoyoshih13@gmail.com>
2019-11-11 18:50:45 -0500

    add example codes for 2d type2 many; rename 2d type 1 example

---
+++ examples/example2d2many.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <complex>
+
+#include "../src/cufinufft.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+/*
+ * example code for 2D Type 1 transformation.
+ *
+ * To compile the code:
+ * 	nvcc example2d2many.cpp -o example2d2many /loc/to/cufinufft/lib-static/libcufinufftf.a -lcudart -lcufft -lnvToolsExt
+ * 
+ * or
+ * export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/loc/to/cufinufft/lib
+ * nvcc example2d2many.cpp -L/loc/to/cufinufft/lib/ -o example2d1 -lcufinufftf
+ *
+ *
+ */
+{
+	cout<<scientific<<setprecision(3);
+
+	int ier;
+	int N1 = 128;
+	int N2 = 128;
+	int M = 10;
+	int ntransf = 4;
+	int ntransfcufftplan = 4;
+	int iflag=1;
+	float tol=1e-6;
+
+	float *x, *y;
+	complex<float> *c, *fk;
+	cudaMallocHost(&x, M*sizeof(float));
+	cudaMallocHost(&y, M*sizeof(float));
+	cudaMallocHost(&c, M*ntransf*sizeof(complex<float>));
+	cudaMallocHost(&fk,N1*N2*ntransf*sizeof(complex<float>));
+
+	float *d_x, *d_y;
+	cuFloatComplex *d_c, *d_fk;
+	cudaMalloc(&d_x,M*sizeof(float));
+	cudaMalloc(&d_y,M*sizeof(float));
+	cudaMalloc(&d_c,M*ntransf*sizeof(cuFloatComplex));
+	cudaMalloc(&d_fk,N1*N2*ntransf*sizeof(cuFloatComplex));
+
+	for (int i=0; i<M; i++) {
+		x[i] = M_PI*randm11();
+		y[i] = M_PI*randm11();
+	}
+
+	for(int i=0; i<M*ntransf; i++){
+		c[i].real() = randm11();
+		c[i].imag() = randm11();
+	}
+	cudaMemcpy(d_x,x,M*sizeof(float),cudaMemcpyHostToDevice);
+	cudaMemcpy(d_y,y,M*sizeof(float),cudaMemcpyHostToDevice);
+	cudaMemcpy(d_c,c,M*ntransf*sizeof(cuFloatComplex),cudaMemcpyHostToDevice);
+
+	cufinufft_plan dplan;
+
+	int dim = 2;
+	int nmodes[3];
+
+	ier=cufinufft_default_opts(type2, dim, dplan.opts);
+
+	nmodes[0] = N1;
+	nmodes[1] = N2;
+	nmodes[2] = 1;
+
+	ier=cufinufft_makeplan(type1, dim, nmodes, iflag, ntransf, tol, 
+		ntransfcufftplan, &dplan);
+
+	ier=cufinufft_setNUpts(M, d_x, d_y, NULL, 0, NULL, NULL, NULL, &dplan);
+
+	ier=cufinufft_exec(d_c, d_fk, &dplan);
+
+	ier=cufinufft_destroy(&dplan);
+
+	cudaMemcpy(fk,d_fk,N1*N2*ntransf*sizeof(cuFloatComplex),cudaMemcpyDeviceToHost);
+
+	cout<<endl<<"Accuracy check:"<<endl;
+	int N = N1*N2;
+	for(int i=0; i<ntransf; i+=5){
+		int nt1 = (int)(0.37*N1), nt2 = (int)(0.26*N2);  // choose some mode index to check
+		complex<float> Ft = complex<float>(0,0), J = IMA*(float)iflag;
+		for (BIGINT j=0; j<M; ++j)
+			Ft += c[j+i*M] * exp(J*(nt1*x[j]+nt2*y[j]));   // crude direct
+		int it = N1/2+nt1 + N1*(N2/2+nt2);   // index in complex F as 1d array
+		printf("[gpu %3d] one mode: abs err in F[%ld,%ld] is %.3g\n",i,(int)nt1,
+			(int)nt2,abs(Ft-fk[it+i*N]));
+		printf("[gpu %3d] one mode: rel err in F[%ld,%ld] is %.3g\n",i,(int)nt1,
+			(int)nt2,abs(Ft-fk[it+i*N])/infnorm(N,fk+i*N));
+	}
+
+	cudaFreeHost(x);
+	cudaFreeHost(y);
+	cudaFreeHost(c);
+	cudaFreeHost(fk);
+
+	cudaFree(d_x);
+	cudaFree(d_y);
+	cudaFree(d_c);
+	cudaFree(d_fk);
+	return 0;
+}
