96b0fa6cace59dffd5b2843378bc2c7aebf7551a
Libin <llu@flatironinstitute.org>
2020-06-03 04:51:25 -0400

    finish matlab finufft1d simple interfaces

---
+++ matlab/finufft1d2.m
@@ -0,0 +1,36 @@
+function [c ier] = finufft1d2(x,isign,eps,f,o)
+% FINUFFT1D2
+%
+% [c ier] = finufft1d2(x,isign,eps,f)
+% [c ier] = finufft1d2(x,isign,eps,f,opts)
+%
+% Type-2 1D complex nonuniform FFT.
+%
+%    c[j] = SUM   f[k1] exp(+/-i k1 x[j])      for j = 1,...,nj
+%            k1
+%     where sum is over -ms/2 <= k1 <= (ms-1)/2.
+%
+%  Inputs:
+%     x     location of NU targets on interval [-3pi,3pi], length nj
+%     f     complex Fourier transform values
+%     isign  if >=0, uses + sign in exponential, otherwise - sign.
+%     eps    precision requested (>1e-16)
+%     opts.debug: 0 (silent, default), 1 (timing breakdown), 2 (debug info).
+%     opts.nthreads sets requested number of threads (else automatic)
+%     opts.spread_sort: 0 (don't sort NU pts), 1 (do), 2 (auto, default)
+%     opts.fftw: 0 (use FFTW_ESTIMATE, default), 1 (use FFTW_MEASURE)
+%     opts.modeord: 0 (CMCL increasing mode ordering, default), 1 (FFT ordering)
+%     opts.chkbnds: 0 (don't check NU points valid), 1 (do, default).
+%     opts.upsampfac: either 2.0 (default), or 1.25 (low RAM, smaller FFT size)
+%  Outputs:
+%     c     complex double array of nj answers at targets
+%     ier - 0 if success, else:
+%           1 : eps too small
+%           2 : size of arrays to malloc exceed MAX_NF
+%           other codes: as returned by cnufftspread
+
+if nargin<5, o=[]; end
+[ms,n_transf]=size(f);
+p = finufft_plan(2,ms,isign,n_transf,eps,o);
+p.finufft_setpts(x,[],[],[],[],[]);
+[c,ier] = p.finufft_exec(f);
---
+++ matlab/finufft1d3.m
@@ -0,0 +1,32 @@
+function [f ier] = finufft1d3(x,c,isign,eps,s,o)
+% FINUFFT1D3
+%
+% [f ier] = finufft1d3(x,c,isign,eps,s)
+% [f ier] = finufft1d3(x,c,isign,eps,s,opts)
+%
+%              nj
+%     f[k]  =  SUM   c[j] exp(+-i s[k] x[j]),      for k = 1, ..., nk
+%              j=1
+%   Inputs:
+%     x      location of NU sources in R (real line).
+%     c      size-nj double complex array of source strengths
+%     s      frequency locations of NU targets in R.
+%     isign  if >=0, uses + sign in exponential, otherwise - sign.
+%     eps    precision requested (>1e-16)
+%     opts.debug: 0 (silent, default), 1 (timing breakdown), 2 (debug info).
+%     opts.nthreads sets requested number of threads (else automatic)
+%     opts.spread_sort: 0 (don't sort NU pts), 1 (do), 2 (auto, default)
+%     opts.fftw: 0 (use FFTW_ESTIMATE, default), 1 (use FFTW_MEASURE)
+%     opts.upsampfac: either 2.0 (default), or 1.25 (low RAM, smaller FFT size)
+%   Outputs:
+%     f     size-nk double complex Fourier transform values at target
+%            frequencies s
+%     returned value - 0 if success, else:
+%                      1 : eps too small
+%                      2 : size of arrays to malloc exceed MAX_NF
+
+if nargin<6, o=[]; end
+n_transf = round(numel(c)/numel(x));
+p = finufft_plan(3,1,isign,n_transf,eps,o);
+p.finufft_setpts(x,[],[],s,[],[]);
+[f,ier] = p.finufft_exec(c);
