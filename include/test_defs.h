5097e1425835b998d3cee1be16f971db50b03c5a
ahbarnett <ahb@math.dartmouth.edu>
2020-06-25 12:37:01 -0400

    I'm an idiot: added test_defs.h

---
+++ include/test_defs.h
@@ -0,0 +1,12 @@
+// test-wide definitions for use in ../test/*.cpp
+
+#ifndef TEST_DEFS_H
+#define TEST_DEFS_H
+
+// how big a problem to check direct DFT for in 1D...
+#define TEST_BIGPROB 1e8
+
+// for omp rand filling
+#define TEST_RANDCHUNK 1000000
+
+#endif   // TEST_DEFS_H
