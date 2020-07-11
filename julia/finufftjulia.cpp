110692a4ddae9691654b2f2736bfb83cb4769c94
Joakim Andén <janden@kth.se>
2020-07-10 22:46:24 -0400

    Update Julia code for new plan type

--- julia/finufftjulia.cpp
+++ julia/finufftjulia.cpp
@@ -17,46 +17,34 @@
 extern "C" {
 #endif
 
-finufft_plan* finufft_plan_alloc()
-{
-  finufft_plan* plan = (finufft_plan*)malloc(sizeof(finufft_plan));
-  return plan;
-}
-
-void finufft_plan_free(finufft_plan* plan)
-{
-  if(plan)
-    free(plan);
-}
-
-int get_type(finufft_plan* plan)
+int get_type(finufft_plan plan)
 {
   return plan->type;
 }
 
-int get_ntransf(finufft_plan* plan)
+int get_ntransf(finufft_plan plan)
 {
   return plan->ntrans;
 }
 
-int get_ndims(finufft_plan* plan)
+int get_ndims(finufft_plan plan)
 {
   return plan->dim;
 }
 
-void get_nmodes(finufft_plan* plan, BIGINT* n_modes)
+void get_nmodes(finufft_plan plan, BIGINT* n_modes)
 {
   n_modes[0] = plan->ms;
   n_modes[1] = plan->mt;
   n_modes[2] = plan->mu;
 }
 
-BIGINT get_nj(finufft_plan* plan)
+BIGINT get_nj(finufft_plan plan)
 {
   return plan->nj;
 }
 
-BIGINT get_nk(finufft_plan* plan)
+BIGINT get_nk(finufft_plan plan)
 {
   return plan->nk;
 }
