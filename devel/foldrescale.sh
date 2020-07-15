9a224c09a04932040b357b09f790e99a9949727c
ahbarnett <ahb@math.dartmouth.edu>
2020-07-15 18:05:35 -0400

    foldrescale perfs tests concluding MACRO beats function - weird

--- devel/foldrescale.sh
+++ devel/foldrescale.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-g++-9 -O3 -ffast-math -march=native -funroll-loops -I../include -fopenmp foldrescale_perf2.cpp -o foldrescale_perf2 -lgomp
-./foldrescale_perf2 1e8
+echo "BINNING (small N)...."
 
 g++-9 -O3 -march=native -funroll-loops -I../include -fopenmp foldrescale_perf2.cpp -o foldrescale_perf2 -lgomp
-./foldrescale_perf2 1e8
+./foldrescale_perf2
+
+g++-9 -O3 -march=native -funroll-loops -I../include -fopenmp foldrescale_perf2.cpp -o foldrescale_perf2 -lgomp -ffast-math -fno-finite-math-only
+./foldrescale_perf2
+
+echo "NOBIN...."
+
+g++-9 -O3 -march=native -funroll-loops -I../include -fopenmp foldrescale_perf2.cpp -o foldrescale_perf2 -lgomp -DNOBIN
+./foldrescale_perf2
+
+g++-9 -O3 -march=native -funroll-loops -I../include -fopenmp foldrescale_perf2.cpp -o foldrescale_perf2 -lgomp -ffast-math -fno-finite-math-only -DNOBIN
+./foldrescale_perf2
