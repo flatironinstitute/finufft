c3d70ccf33b42b81d77a724bc4d1dc462908afd9
ahbarnett <ahb@math.dartmouth.edu>
2020-07-15 15:12:11 -0400

    foldrescale perf test re/unscale inputs so same # conditionals

--- devel/foldrescale_perf2.cpp
+++ devel/foldrescale_perf2.cpp
@@ -3,7 +3,7 @@
 
    Compile with, eg on linux, double-prec:
 
-   g++-7 -O3 -funroll-loops -march=native -I../include -fopenmp foldrescale_perf2.cpp -o foldrescale_perf2 -lgomp
+   g++-9 -O3 -funroll-loops -march=native -I../include -fopenmp foldrescale_perf2.cpp -o foldrescale_perf2 -lgomp
    Use -DSINGLE for single-prec. OMP only used for random # gen.
 
    Usage:
@@ -11,13 +11,13 @@
    M = number of calls to rescale/fold, N = grid size (not known at compile time)
 
    Examples
-   ./foldrescale_perf 1e8
-   ./foldrescale_perf 1e8 1e6
+   ./foldrescale_perf2 1e8
+   ./foldrescale_perf2 1e8 1e6
 
    Barnett 7/15/20
 
 
-i7 GCC RESULTS:  ./foldrescale.sh:  fast-math then not
+i7 GCC RESULTS:  ./foldrescale.sh:  fast-math, then not
 
 simple array sum:           	0.4 ns/call	(sum:81753.8964958474)
 w/ RESCALE macro (pir=0):	0.8 ns/call	(sum:49992036081751.82)
@@ -63,8 +63,6 @@ FLT foldrescale(FLT x, BIGINT N, int pirange)
   FLT z = x;
   if (pirange)
     z = (N/(2*PI)) * (x+PI);                  // PI is (FLT)M_PI in defs.h
-  else
-    z = x;
   // fold...
   if (z<(FLT)0.0)
     z += (FLT)N;
@@ -89,9 +87,10 @@ int main(int argc, char* argv[])
     unsigned int s=omp_get_thread_num();  // needed for parallel random #s
 #pragma omp for schedule(dynamic,1000000)
     for (int i=0; i<M; ++i)
-      x[i]=-3.0*PI*randm11r(&s);          // unif over the folded domain
+      x[i] = 3.0*PI*randm11r(&s);          // unif over the folded domain
   }
   // (note when pirange=0 the conditional <0 vs >=0 still 1:2 random)
+  // We'll reuse this array by rescaling/unrescaling by hand.
   
   FLT sum=0.0;
   auto tbegin = system_clock::now();
@@ -100,22 +99,32 @@ int main(int argc, char* argv[])
   duration<double> dur = system_clock::now() - tbegin;   // dur.count() is sec
   printf("simple array sum:           \t%.1f ns/call\t(sum:%.16g)\n",1e9*dur.count()/(double)M,sum);
 
-  sum = 0.0;
   for (int pirange=0;pirange<2;++pirange) {
+    if (!pirange)
+      for (int i=0;i<M;++i) x[i] = (N/(2*PI)) * (x[i]+PI);   // rescale to [0,N)
+    //FLT mx=0.0; for (int i=0;i<M;++i) if (x[i]>mx) mx=x[i];   // chk max
+    //printf("max x=%.3g\n",mx);
+    sum = 0.0;
     tbegin = system_clock::now();
     for (int i=0;i<M;++i)
       sum += RESCALE(x[i],N,pirange);
     dur = system_clock::now() - tbegin;   // dur.count() is sec
     printf("w/ RESCALE macro (pir=%d):\t%.1f ns/call\t(sum:%.16g)\n",pirange,1e9*dur.count()/(double)M,sum);
+    if (!pirange)
+      for (int i=0;i<M;++i) x[i] = x[i]*((2*PI)/N) - PI;   // undo rescale
   }
   
-  sum = 0.0;
   for (int pirange=0;pirange<2;++pirange) {
+    if (!pirange)
+      for (int i=0;i<M;++i) x[i] = (N/(2*PI)) * (x[i]+PI);   // rescale to [0,N)
+    sum = 0.0;
     tbegin = system_clock::now();
     for (int i=0;i<M;++i)
       sum += foldrescale(x[i],N,pirange);
     dur = system_clock::now() - tbegin;   // dur.count() is sec
     printf("w/ foldrescale (pir=%d):  \t%.1f ns/call\t(sum:%.16g)\n",pirange,1e9*dur.count()/(double)M,sum);
+    if (!pirange)
+      for (int i=0;i<M;++i) x[i] = x[i]*((2*PI)/N) - PI;   // undo rescale
   }
   
   return 0;
