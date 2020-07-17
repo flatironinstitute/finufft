af2f87aca315ff05da4d6a4d4d1dd9942bc5cb58
Garrett Wright <garrettwrong@gmail.com>
2020-07-17 14:30:27 -0400

    mkdir ci and build-wheel.sh stub

---
+++ ci/build-wheels.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e -u -x
+
+function repair_wheel {
+    wheel="$1"
+    if ! auditwheel show "$wheel"; then
+        echo "Skipping non-platform wheel $wheel"
+    else
+        auditwheel repair "$wheel" --plat "$PLAT" -w /io/wheelhouse/
+    fi
+}
+
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
+done
+
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    repair_wheel "$whl"
+done
+
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install cufinufftpy -f /io/wheelhouse
+done
