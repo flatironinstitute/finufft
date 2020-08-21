merged our guru2d1{f}.py; fixed dollar sign around PYTHON in makefile

python/examples/guru2d1.py
@@ -1,4 +1,4 @@
-# demo of vectorized 2D type 1 FINUFFT in python for guru interface. Should stay close to docs/python.rst
+# demo of vectorized 2D type 1 FINUFFT in python via guru interface. Should stay close to docs/python.rst
 # Lu 8/20/20
 
 import numpy as np
@@ -14,7 +14,7 @@ x = 2 * np.pi * np.random.uniform(size=M)
 y = 2 * np.pi * np.random.uniform(size=M)
 
 # number of transforms
-K = 7
+K = 4
 
 # generate K stacked strength arrays
 c = (np.random.standard_normal(size=(K, M))
@@ -24,23 +24,21 @@ c = (np.random.standard_normal(size=(K, M))
 N1 = 1000
 N2 = 2000
 
-# guru interface calls to calculate the K transforms simultaneously
-t0 = time.time()
-
 # specify type 1 transform
 nufft_type = 1
 
-# instantiate the plan (note ntrans must be set here)
-plan = finufftpy.Plan(nufft_type, (N1, N2), eps=1e-12, n_trans=K)
+# instantiate the plan (note n_trans must be set here), also setting tolerance:
+t0 = time.time()
+plan = finufftpy.Plan(nufft_type, (N1, N2), eps=1e-9, n_trans=K)
 
 # set the nonuniform points
 plan.setpts(x, y)
 
-# execute the plan
+# execute the plan (K transforms together, note c.shape must match)
 f = plan.execute(c)
-print(f.dtype)
+print("vectorized guru finufft2d1 done in {0:.2g} s.".format(time.time()-t0))
 
-print("vectorized finufft2d1 done in {0:.2g} s.".format(time.time()-t0))
+print(f.dtype)
 print(f.shape)
 
 k1 = 376     # do a math check, for a single output mode index (k1,k2)