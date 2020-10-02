7d1a254805e85bc5acd17898896b270e3196e1fd
Garrett Wright <garrettwrong@gmail.com>
2020-10-02 11:14:47 -0400

    Reorder pts args in wrapper to match FINUFFT.

--- python/cufinufft/cufinufft.py
+++ python/cufinufft/cufinufft.py
@@ -48,7 +48,7 @@ class cufinufft:
 
         :param finufft_type: integer 1, 2, or 3.
         :param modes: Array describing the shape of the transform \
-        in 1, 2, or 3 dimensions. (ZYX order, same as ndarray.shape)
+        in 1, 2, or 3 dimensions.
         :param isign: 1 or -1, controls sign of imaginary component output.
         :param tol: Floating point tolerance.
         :param ntransforms: Number of transforms, defaults to 1.
@@ -93,10 +93,8 @@ class cufinufft:
         self._maxbatch = 1    # TODO: optimize this one day
 
         # We extend the mode tuple to 3D as needed,
-        #   and reorder from python ndarray.shape style input (nZ, nY, nX)
-        #   to the order expected by the underlying call (Nx, Ny, Nz).
-        # The underlying C++ args are labeled (..., X, Y, Z, ...)
-        #   and (...., N1, N2, N3, ....) so no convention is needed there.
+        #   and reorder from C/python ndarray.shape style input (nZ, nY, nX)
+        #   to the (F) order expected by the low level library (nX, nY, nZ).
         modes = modes[::-1] + (1,) * (3 - self.dim)
         self.modes = (c_int * 3)(*modes)
 
@@ -170,15 +168,25 @@ class cufinufft:
             raise TypeError("cufinufft plan.dtype and "
                             "kz dtypes do not match.")
 
-        kx = kx.ptr
+        # Because FINUFFT/cufinufft are internally column major,
+        #   we will reorder the pts axes. Reordering references
+        #   save us from having to actually transpose signal data
+        #   from row major (Python default) to column major.
+        #   We do this by following translation:
+        #     (x, None, None) ~>  (x, None, None)
+        #     (x, y, None)    ~>  (y, x, None)
+        #     (x, y, z)       ~>  (z, y, x)
+        # Via code, we push each dimension onto a stack of axis
+        fpts_axes = [kx.ptr, None, None]
 
         if ky is not None:
-            ky = ky.ptr
+            fpts_axes.insert(0, ky.ptr)
 
         if kz is not None:
-            kz = kz.ptr
+            fpts_axes.insert(0, kz.ptr)
 
-        ier = self._set_pts(M, kx, ky, kz, 0, None, None, None, self.plan)
+        # Then take three items off the stack as our reordered axis.
+        ier = self._set_pts(M, *fpts_axes[:3], 0, None, None, None, self.plan)
 
         if ier != 0:
             raise RuntimeError('Error setting non-uniform points.')
