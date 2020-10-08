caee26587cd1e55841ab2830f1d14e9b97a37ddb
ahbarnett <ahb@math.dartmouth.edu>
2020-10-08 18:38:13 -0400

    cleaned old code from py/test; rebuilt docs pdf

--- devel/non-contiguous_frequency_inputs_bug2d1.py
+++ devel/non-contiguous_frequency_inputs_bug2d1.py
@@ -1,8 +1,10 @@
+# ahb updated for v2.0 interface, and complex c. Bug seems to have been fixed :)
+
 import numpy as np
 
 from finufft import nufft2d1
 
-c = np.arange(2)
+c = np.complex128(np.random.rand(2))
 
 omega = np.arange(4).reshape((2, 2)) / 3 * np.pi
 
@@ -11,13 +13,13 @@ y0 = omega[:, 1]
 
 f0 = np.zeros((4, 4), order='F', dtype=np.complex128)
 
-nufft2d1(x0, y0, c, 1, 1e-15, f0.shape[0], f0.shape[1], f0)
+nufft2d1(x0, y0, c, f0.shape, out=f0, eps=1e-14)
 
 x1 = x0.copy()
 y1 = y0.copy()
 
 f1 = np.zeros((4, 4), order='F', dtype=np.complex128)
 
-nufft2d1(x1, y1, c, 1, 1e-15, f1.shape[0], f1.shape[1], f1)
+nufft2d1(x1, y1, c, f1.shape, out=f1, eps=1e-14)
 
 print('difference: %e' % np.linalg.norm(f0 - f1))
--- python/test/README.md
+++ python/test/README.md
@@ -1,14 +1,14 @@
 ## Accuracy and speed tests using the python wrappers
 
-To install the python (or python3) wrappers for finufft, see ../docs/install.rst
+To install the python wrappers for finufft
+see ../../docs/install.rst
 
 Then you may run the tests as follows
 
 ```
-python3 demo1d1.py
 python3 run_accuracy_tests.py
 python3 run_speed_tests.py
 ```
 
-The codes `demo1d1.py` and `accuracy_speed_tests.py` illustrate how to call
+The codes `accuracy_speed_tests.py` and `../examples/*` illustrate how to call
 FINUFFT from python.
