bdd23cd3a61cc552f94ac4834e24852eaa83058a
Libin <llu@flatironinstitute.org>
2021-09-16 14:17:31 -0400

    added python wheel ci for linux

---
+++ python/ci/build-wheels-linux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e -x
+
+# Replace native compilation flags with more generic ones.
+cp make.inc.manylinux make.inc
+
+# Clean up the build and make the library.
+make clean
+make lib
+
+# Test to make sure everything is ok.
+make test
+
+# Needed for pip install to work
+export FINUFFT_DIR=$(pwd)
+# Needed for auditwheel to find the dynamic libraries
+export LD_LIBRARY_PATH=${FINUFFT_DIR}/lib:${LD_LIBRARY_PATH}
+
+pys=(/opt/python/*/bin)
+
+# Filter out old Python versions
+pys=(${pys[@]//*27*/})
+pys=(${pys[@]//*34*/})
+pys=(${pys[@]//*35*/})
+
+for PYBIN in "${pys[@]}"; do
+    "${PYBIN}/pip" install auditwheel wheel twine numpy
+    "${PYBIN}/pip" wheel ./python -w python/wheelhouse
+done
+
+for whl in python/wheelhouse/finufft-*.whl; do
+    auditwheel repair "$whl" -w python/wheelhouse/
+done
