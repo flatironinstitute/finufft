b29d10fb80c3cb6a51ca74a9542c3634b2b4abbb
MelodyShih <yoyoshih13@gmail.com>
2021-11-22 11:32:11 -0500

    (wip) add 1d tests codes

---
+++ test/cufinufft1d1_test.cu
@@ -0,0 +1,179 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <helper_cuda.h>
+#include <complex>
+
+#include <cufinufft_eitherprec.h>
+
+#include "../contrib/utils.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int N1, M, N;
+	if (argc<3) {
+		fprintf(stderr,
+			"Usage: cufinufft1d1_test method N1 [M [tol]]\n"
+			"Arguments:\n"
+			"  method: One of\n"
+			"    1: nupts driven, or\n"
+			"    2: sub-problem\n"
+			"  N1: The size of the 1D array.\n"
+			"  M: The number of non-uniform points (default N1).\n"
+			"  tol: NUFFT tolerance (default 1e-6).\n");
+		return 1;
+	}
+	double w;
+	int method;
+	sscanf(argv[1],"%d",&method);
+	sscanf(argv[2],"%lf",&w); N1 = (int)w;  // so can read 1e6 right!
+	N = N1;
+	M = N1;// let density always be 1
+	if(argc>3){
+		sscanf(argv[3],"%lf",&w); M  = (int)w;  // so can read 1e6 right!
+	}
+
+	FLT tol=1e-6;
+	if(argc>4){
+		sscanf(argv[4],"%lf",&w); tol  = (FLT)w;  // so can read 1e6 right!
+	}
+	int iflag=1;
+
+
+	cout<<scientific<<setprecision(3);
+	int ier;
+
+
+	FLT *x;
+	CPX *c, *fk;
+	cudaMallocHost(&x, M*sizeof(FLT));
+	cudaMallocHost(&c, M*sizeof(CPX));
+	cudaMallocHost(&fk,N1*sizeof(CPX));
+
+	FLT *d_x;
+	CUCPX *d_c, *d_fk;
+	checkCudaErrors(cudaMalloc(&d_x,M*sizeof(FLT)));
+	checkCudaErrors(cudaMalloc(&d_c,M*sizeof(CUCPX)));
+	checkCudaErrors(cudaMalloc(&d_fk,N1*sizeof(CUCPX)));
+
+	// Making data
+	for (int i = 0; i < M; i++) {
+		x[i] = M_PI*randm11();// x in [-pi,pi)
+		c[i].real(randm11());
+		c[i].imag(randm11());
+	}
+
+	checkCudaErrors(cudaMemcpy(d_x,x,M*sizeof(FLT),cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_c,c,M*sizeof(CPX),cudaMemcpyHostToDevice));
+
+	cudaEvent_t start, stop;
+	float milliseconds = 0;
+	float totaltime = 0;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	// warm up CUFFT (is slow, takes around 0.2 sec... )
+	cudaEventRecord(start);
+ 	{
+		int nf1=1;
+		cufftHandle fftplan;
+		cufftPlan1d(&fftplan,nf1,CUFFT_TYPE,1);
+	}
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&milliseconds, start, stop);
+	printf("[time  ] dummy warmup call to CUFFT\t %.3g s\n", milliseconds/1000);
+
+	// now to our tests...
+	CUFINUFFT_PLAN dplan;
+	int dim = 1;
+	int type = 1;
+
+	// Here we setup our own opts, for gpu_method.
+	cufinufft_opts opts;
+	ier=CUFINUFFT_DEFAULT_OPTS(type, dim, &opts);
+	if(ier!=0){
+	  printf("err %d: CUFINUFFT_DEFAULT_OPTS\n", ier);
+	  return ier;
+	}
+
+	opts.gpu_method=method;
+
+	int nmodes[3];
+	int ntransf = 1;
+	int maxbatchsize = 1;
+	nmodes[0] = N1;
+	nmodes[1] = 1;
+	nmodes[2] = 1;
+	cudaEventRecord(start);
+	ier=CUFINUFFT_MAKEPLAN(type, dim, nmodes, iflag, ntransf, tol,
+			       maxbatchsize, &dplan, &opts);
+	if (ier!=0){
+	  printf("err: cufinufft1d_plan\n");
+	  return ier;
+	}
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&milliseconds, start, stop);
+	totaltime += milliseconds;
+	printf("[time  ] cufinufft plan:\t\t %.3g s\n", milliseconds/1000);
+
+
+	cudaEventRecord(start);
+	ier=CUFINUFFT_SETPTS(M, d_x, NULL, NULL, 0, NULL, NULL, NULL, dplan);
+	if (ier!=0){
+	  printf("err: cufinufft_setpts\n");
+	  return ier;
+	}
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&milliseconds, start, stop);
+	totaltime += milliseconds;
+	printf("[time  ] cufinufft setNUpts:\t\t %.3g s\n", milliseconds/1000);
+
+
+	cudaEventRecord(start);
+	ier=CUFINUFFT_EXECUTE(d_c, d_fk, dplan);
+	if (ier!=0){
+	  printf("err: cufinufft1d1_exec\n");
+	  return ier;
+	}
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&milliseconds, start, stop);
+	totaltime += milliseconds;
+	float exec_ms = milliseconds;
+	printf("[time  ] cufinufft exec:\t\t %.3g s\n", milliseconds/1000);
+
+	cudaEventRecord(start);
+	ier=CUFINUFFT_DESTROY(dplan);
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&milliseconds, start, stop);
+	totaltime += milliseconds;
+	printf("[time  ] cufinufft destroy:\t\t %.3g s\n", milliseconds/1000);
+
+	checkCudaErrors(cudaMemcpy(fk,d_fk,N1*sizeof(CUCPX), cudaMemcpyDeviceToHost));
+
+	printf("[Method %d] %d NU pts to %d U pts in %.3g s:      %.3g NU pts/s\n",
+			opts.gpu_method,M,N1,totaltime/1000,M/totaltime*1000);
+	printf("\t\t\t\t\t(exec-only thoughput: %.3g NU pts/s)\n",M/exec_ms*1000);
+
+	int nt1 = (int)(0.37*N1);  // choose some mode index to check
+	CPX Ft = CPX(0,0), J = IMA*(FLT)iflag;
+	for (BIGINT j=0; j<M; ++j)
+		Ft += c[j] * exp(J*(nt1*x[j]));   // crude direct
+	int it = N1/2+nt1;   // index in complex F as 1d array
+//	printf("[gpu   ] one mode: abs err in F[%ld is %.3g\n",(int)nt1,abs(Ft-fk[it]));
+	printf("[gpu   ] one mode: rel err in F[%ld] is %.3g\n",(int)nt1,abs(Ft-fk[it])/infnorm(N,fk));
+
+	cudaFreeHost(x);
+	cudaFreeHost(c);
+	cudaFreeHost(fk);
+	cudaFree(d_x);
+	cudaFree(d_c);
+	cudaFree(d_fk);
+	return 0;
+}
---
+++ test/spread1d_test.cu
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <helper_cuda.h>
+#include <complex>
+#include <algorithm>
+#include "../src/cuspreadinterp.h"
+#include "../contrib/utils.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int nf1, N1, M;
+	FLT upsampfac=2.0;
+	if (argc<4) {
+		fprintf(stderr,
+			"Usage: spread1d method nupts_distr nf1 [maxsubprobsize [M [tol [kerevalmeth]]]]\n"
+			"Arguments:\n"
+			"  method: One of\n"
+			"    1: nupts driven, or\n"
+			"    2: sub-problem\n"
+			"  nupts_distr: The distribution of the points; one of\n"
+			"    0: uniform, or\n"
+			"    1: concentrated in a small region.\n"
+			"  nf1: The size of the 1D array.\n"
+			"  maxsubprobsize: Maximum size of subproblems (default 65536).\n"
+			"  M: The number of non-uniform points (default nf1 / 2).\n"
+			"  tol: NUFFT tolerance (default 1e-6).\n"
+			"  kerevalmeth: Kernel evaluation method; one of\n"
+			"     0: Exponential of square root (default), or\n"
+			"     1: Horner evaluation.\n");
+		return 1;
+	}
+	double w;
+	int method;
+	sscanf(argv[1],"%d",&method);
+
+	int nupts_distribute;
+	sscanf(argv[2],"%d",&nupts_distribute);
+	sscanf(argv[3],"%lf",&w); nf1 = (int)w;  // so can read 1e6 right!
+
+	int maxsubprobsize=65536;
+	if(argc>4){
+		sscanf(argv[4],"%d",&maxsubprobsize);
+	}
+
+	N1 = (int) nf1/upsampfac;
+	M = N1;
+	if(argc>5){
+		sscanf(argv[5],"%lf",&w); M  = (int)w;  // so can read 1e6 right!
+	}
+
+	FLT tol=1e-6;
+	if(argc>6){
+		sscanf(argv[6],"%lf",&w); tol  = (FLT)w;  // so can read 1e6 right!
+	}
+
+	int kerevalmeth=0;
+	if(argc>7){
+		sscanf(argv[7],"%d",&kerevalmeth);
+	}
+
+	int ier;
+	int dim=1;
+
+	CUFINUFFT_PLAN dplan = new CUFINUFFT_PLAN_S;
+        // Zero out your struct, (sets all pointers to NULL, crucial)
+        memset(dplan, 0, sizeof(*dplan));
+	ier = CUFINUFFT_DEFAULT_OPTS(1 /*type*/, dim, &(dplan->opts));
+
+	dplan->opts.gpu_method           = method;
+	dplan->opts.gpu_maxsubprobsize   = maxsubprobsize;
+	dplan->opts.gpu_kerevalmeth      = kerevalmeth;
+	dplan->opts.gpu_sort             = 1;  // ahb changed from 0
+	dplan->opts.gpu_spreadinterponly = 1;
+	dplan->opts.gpu_binsizex         = 32; //binsize needs to be set here, since
+                                           //SETUP_BINSIZE() is not called in 
+                                           //spread, interp only wrappers.
+	ier = setup_spreader_for_nufft(dplan->spopts, tol, dplan->opts);
+
+	cout<<scientific<<setprecision(3);
+
+	FLT *x;
+	CPX *c, *fw;
+	cudaMallocHost(&x, M*sizeof(FLT));
+	cudaMallocHost(&c, M*sizeof(CPX));
+	cudaMallocHost(&fw,nf1*sizeof(CPX));
+
+	FLT *d_x;
+	CUCPX *d_c, *d_fw;
+	checkCudaErrors(cudaMalloc(&d_x,M*sizeof(FLT)));
+	checkCudaErrors(cudaMalloc(&d_c,M*sizeof(CUCPX)));
+	checkCudaErrors(cudaMalloc(&d_fw,nf1*sizeof(CUCPX)));
+
+	switch(nupts_distribute){
+		// Making data
+		case 0: //uniform
+			{
+				for (int i = 0; i < M; i++) {
+					x[i] = M_PI*randm11();// x in [-pi,pi)
+					c[i].real(randm11());
+					c[i].imag(randm11());
+				}
+			}
+			break;
+		case 1: // concentrate on a small region
+			{
+				for (int i = 0; i < M; i++) {
+					x[i] = M_PI*rand01()/(nf1*2/32);
+					c[i].real(randm11());
+					c[i].imag(randm11());
+				}
+			}
+			break;
+		default:
+			cerr << "not valid nupts distr" << endl;
+	}
+
+	checkCudaErrors(cudaMemcpy(d_x,x,M*sizeof(FLT),cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_c,c,M*sizeof(CUCPX),cudaMemcpyHostToDevice));
+
+	CNTime timer;
+	timer.restart();
+	ier = CUFINUFFT_SPREAD1D(nf1, d_fw, M, d_x, d_c, dplan);
+	if(ier != 0 ){
+		cout<<"error: cnufftspread2d"<<endl;
+		return 0;
+	}
+	FLT t=timer.elapsedsec();
+	printf("[Method %d] %ld NU pts to #%d U pts in %.3g s (%.3g NU pts/s)\n",
+			dplan->opts.gpu_method,M,nf1,t,M/t);
+
+	checkCudaErrors(cudaMemcpy(fw,d_fw,nf1*sizeof(CUCPX),
+		cudaMemcpyDeviceToHost));
+#ifdef RESULT
+	cout<<"[result-input]"<<endl;
+	for (int i=0; i<nf1; i++){
+		if( i % dplan->opts.gpu_binsizex == 0 && i!=0)
+			printf(" |");
+		printf(" (%2.3g,%2.3g)",fw[i].real(),fw[i].imag() );
+	}
+#endif
+
+	cudaFreeHost(x);
+	cudaFreeHost(c);
+	cudaFreeHost(fw);
+	cudaFree(d_x);
+	cudaFree(d_c);
+	cudaFree(d_fw);
+	return 0;
+}
