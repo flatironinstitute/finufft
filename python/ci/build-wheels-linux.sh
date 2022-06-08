38643e4f9bf4256f2a7d72c217a309d5df8cc46e
Joakim Andén <janden@kth.se>
2022-06-08 17:57:11 +0200

    Limit Python versions used for wheels

--- python/ci/build-wheels-linux.sh
+++ python/ci/build-wheels-linux.sh
@@ -17,13 +17,19 @@ export FINUFFT_DIR=$(pwd)
 # Needed for auditwheel to find the dynamic libraries
 export LD_LIBRARY_PATH=${FINUFFT_DIR}/lib:${LD_LIBRARY_PATH}
 
-pys=(/opt/python/*/bin)
+# Explicitly list Python versions to build
+versions=("cp36-cp36m"
+          "cp37-cp37m"
+          "cp38-cp38"
+          "cp39-cp39"
+          "cp310-cp310"
+          "pp37-pypy37_pp73"
+          "pp38-pypy38_pp73")
 
-# Filter out old Python versions
-pys=(${pys[@]//*27*/})
-pys=(${pys[@]//*34*/})
-pys=(${pys[@]//*35*/})
-pys=(${pys[@]//*pp39-pypy39_pp73*/})
+pys=()
+for version in "${versions[@]}"; do
+    pys+=("/opt/python/${version}/bin")
+done
 
 # build wheel
 for PYBIN in "${pys[@]}"; do
