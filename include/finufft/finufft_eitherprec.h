04ac2555cb9e779017be3b3b28b0c5c5cee981a4
ahbarnett <alex.h.barnett@gmail.com>
2022-08-08 16:07:24 -0400

    finufft_plan_s made private in the C/C++ headers, initial test ok

--- include/finufft/finufft_eitherprec.h
+++ include/finufft/finufft_eitherprec.h
@@ -6,7 +6,7 @@
  safe, eg FINUFFTsomething.
  2)  They will clobber any prior macros starting FINUFFT*, so in the lib/test
  sources finufft.h must be included before defs.h
- 3) for debug, see finufft.h
+ 3) for debug of header macros, see finufft.h
 */
 
 // Local precision-switching macros to make the public interface...
@@ -33,87 +33,10 @@
 #endif
 #define FINUFFT_CPX FINUFFT_COMPLEXIFY(FINUFFT_FLT)
 
-
-//////////////////////////////////////////////////////////////////////
-// finufft_plan struct (plan C-style) for this precision...
+// opaque pointer to finufft_plan private object, for this precision...
 #define FINUFFT_PLAN FINUFFTIFY(_plan)
+// the plan object pointed to... (doesn't need to be even defined here)
 #define FINUFFT_PLAN_S FINUFFTIFY(_plan_s)
-#define FINUFFT_TYPE3PARAMS FINUFFTIFY(_type3Params)
-
-// the plan handle that we pass around is just a pointer to the struct that
-// contains all the info
-typedef struct FINUFFT_PLAN_S * FINUFFT_PLAN;
-
-// group together a bunch of type 3 rescaling/centering/phasing parameters:
-typedef struct {
-  FINUFFT_FLT X1,C1,D1,h1,gam1;  // x dim: X=halfwid C=center D=freqcen h,gam=rescale
-  FINUFFT_FLT X2,C2,D2,h2,gam2;  // y
-  FINUFFT_FLT X3,C3,D3,h3,gam3;  // z
-} FINUFFT_TYPE3PARAMS;
-
-// only the aspects of FFTW needed in this public-facing header...
-#include <fftw3.h>          // (must come after complex.h)
-// (following were typedefed in v<=2.0.4, which seems bad for plain macros)
-#ifdef FINUFFT_SINGLE
-#define FINUFFT_FFTW_CPX fftwf_complex
-#define FINUFFT_FFTW_PLAN fftwf_plan
-#else
-#define FINUFFT_FFTW_CPX fftw_complex
-#define FINUFFT_FFTW_PLAN fftw_plan
-#endif
-
-typedef struct FINUFFT_PLAN_S {  // the main plan struct; note C-compatible struct
-  
-  int type;        // transform type (Rokhlin naming): 1,2 or 3
-  int dim;         // overall dimension: 1,2 or 3
-  int ntrans;      // how many transforms to do at once (vector or "many" mode)
-  FINUFFT_BIGINT nj;  // num of NU pts in type 1,2 (for type 3, num input x pts)
-  FINUFFT_BIGINT nk;  // number of NU freq pts (type 3 only)
-  FINUFFT_FLT tol; // relative user tolerance
-  int batchSize;   // # strength vectors to group together for FFTW, etc
-  int nbatch;      // how many batches done to cover all ntrans vectors
-  
-  FINUFFT_BIGINT ms; // number of modes in x (1) dir (historical CMCL name) = N1
-  FINUFFT_BIGINT mt; // number of modes in y (2) direction = N2
-  FINUFFT_BIGINT mu; // number of modes in z (3) direction = N3
-  FINUFFT_BIGINT N;  // total # modes (prod of above three)
-  
-  FINUFFT_BIGINT nf1;   // size of internal fine grid in x (1) direction
-  FINUFFT_BIGINT nf2;   // " y
-  FINUFFT_BIGINT nf3;   // " z
-  FINUFFT_BIGINT nf;    // total # fine grid points (product of the above three)
-  
-  int fftSign;     // sign in exponential for NUFFT defn, guaranteed to be +-1
-
-  FINUFFT_FLT* phiHat1;    // FT of kernel in t1,2, on x-axis mode grid
-  FINUFFT_FLT* phiHat2;    // " y-axis.
-  FINUFFT_FLT* phiHat3;    // " z-axis.
-  
-  FINUFFT_FFTW_CPX* fwBatch;    // (batches of) fine grid(s) for FFTW to plan
-                                // & act on. Usually the largest working array
-  
-  FINUFFT_BIGINT *sortIndices;  // precomputed NU pt permutation, speeds spread/interp
-  bool didSort;         // whether binsorting used (false: identity perm used)
-
-  FINUFFT_FLT *X, *Y, *Z;  // for t1,2: ptr to user-supplied NU pts (no new allocs).
-                   // for t3: allocated as "primed" (scaled) src pts x'_j, etc
-
-  // type 3 specific
-  FINUFFT_FLT *S, *T, *U;  // pointers to user's target NU pts arrays (no new allocs)
-  FINUFFT_CPX* prephase;   // pre-phase, for all input NU pts
-  FINUFFT_CPX* deconv;     // reciprocal of kernel FT, phase, all output NU pts
-  FINUFFT_CPX* CpBatch;    // working array of prephased strengths
-  FINUFFT_FLT *Sp, *Tp, *Up;  // internal primed targs (s'_k, etc), allocated
-  FINUFFT_TYPE3PARAMS t3P; // groups together type 3 shift, scale, phase, parameters
-  FINUFFT_PLAN innerT2plan;   // ptr used for type 2 in step 2 of type 3
-  
-  // other internal structs; each is C-compatible of course
-  FINUFFT_FFTW_PLAN fftwPlan;
-  finufft_opts opts;     // this and spopts could be made ptrs
-  finufft_spread_opts spopts;
-  
-} FINUFFT_PLAN_S;
-
 
 
 ////////////////////////////////////////////////////////////////////
@@ -123,6 +46,12 @@ extern "C"
 {
 #endif
 
+// ----------------- the plan ----------------------------------------------- 
+// the plan handle that we pass around is just a pointer to the plan object
+// that contains all the info. The latter is invisible to the public user.
+typedef struct FINUFFT_PLAN_S * FINUFFT_PLAN;
+
+  
 // ------------------ the guru interface ------------------------------------
 // (sources in finufft.cpp)
   
