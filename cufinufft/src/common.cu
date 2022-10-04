2ccb9fbe5544094a2b6d4d1cfc124c09e7a63496
Robert Blackwell <rblackwell@flatironinstitute.org>
2022-10-04 13:52:33 -0400

    cuda: remove unnecessary "using"

--- cufinufft/src/common.cu
+++ cufinufft/src/common.cu
@@ -21,7 +21,7 @@ namespace cufinufft {
 namespace common {
 using namespace cufinufft::spreadinterp;
 using std::max;
-using std::min;
+
 /* Kernel for computing approximations of exact Fourier series coeffs of
    cnufftspread's real symmetric kernel. */
 // a , f are intermediate results from function onedim_fseries_kernel_precomp()
