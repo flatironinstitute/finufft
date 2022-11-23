91ff053ad1cffa14262bd28a856bf09e26bafc23
lu1and10 <llu@flatironinstitute.org>
2022-11-24 02:30:09 +0800

    add python3.11 wheel ci

--- python/ci/build-wheels-linux.sh
+++ python/ci/build-wheels-linux.sh
@@ -23,6 +23,7 @@ versions=("cp36-cp36m"
           "cp38-cp38"
           "cp39-cp39"
           "cp310-cp310"
+          "cp311-cp311"
           "pp37-pypy37_pp73"
           "pp38-pypy38_pp73")
 
