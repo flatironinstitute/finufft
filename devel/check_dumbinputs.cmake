15a0841e026dd72d5c47c0d854e213e207d75d2c
ahbarnett <alex.h.barnett@gmail.com>
2023-06-14 17:58:35 -0400

    make ctest actually verify dumbinputs{f} against reference output, fixes #253

---
+++ devel/check_dumbinputs.cmake
@@ -0,0 +1,14 @@
+# platform-indep script to run dumbinputs and check (diff) result
+# used by test/CMakeLists.txt
+
+# pick place for stdout to be saved
+set(tempout
+  "${CMAKE_CURRENT_BINARY_DIR}/dumbinputs${SUFFIX}.out"
+  )
+
+# pipe dumbinputs there. Is it platform-indep?
+# https://stackoverflow.com/questions/36304289/how-to-use-redirection-in-cmake-add-test
+execute_process(COMMAND ${CMAKE_CURRENT_BINARY_DIR}/dumbinputs > ${tempout})
+
+# diff the output against reference
+execute_process(COMMAND ${CMAKE_COMMAND} -E compare_files ${tempout} ${CMAKE_CURRENT_SOURCE_DIR}/results/dumbinputs.refout)
