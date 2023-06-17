fca01ca63275908ce1dd9393539fe1693e3ea039
Joakim Andén <janden@kth.se>
2023-06-16 21:35:39 -0500

    tests: test cufinufft output order check

--- python/cufinufft/tests/test_array_ordering.py
+++ python/cufinufft/tests/test_array_ordering.py
@@ -38,3 +38,26 @@ def test_type2_ordering(dtype=np.float32, shape=(16, 16, 16), M=4096, tol=1e-3):
     # cTT = cTT_gpu.get()
 
     # assert np.allclose(c, cTT, rtol=1e-2)
+
+
+def test_type1_ordering(dtype=np.float32, shape=(16, 16, 16), M=4096, tol=1e-3):
+    complex_dtype = utils._complex_dtype(dtype)
+
+    k, c = utils.type1_problem(dtype, shape, M)
+
+    k_gpu = gpuarray.to_gpu(k)
+    c_gpu = gpuarray.to_gpu(c)
+
+    plan = Plan(1, shape, eps=tol, dtype=complex_dtype)
+
+    plan.setpts(*k_gpu)
+
+    out_gpu = gpuarray.GPUArray(shape, dtype=complex_dtype)
+
+    plan.execute(c_gpu, out=out_gpu)
+
+    out_gpu = gpuarray.GPUArray(shape, dtype=complex_dtype, order="F")
+
+    with pytest.raises(TypeError) as err:
+        plan.execute(c_gpu, out=out_gpu)
+    assert "following requirement: C" in err.value.args[0]
