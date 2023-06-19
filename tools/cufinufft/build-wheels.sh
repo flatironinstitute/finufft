5054eac5124a1a86808d511855875dd078505b7e
Joakim Andén <janden@kth.se>
2023-06-19 15:58:42 -0600

    cuda: specify py versions for wheels
    Since manylinux2014 includes the prerelease version of py312 (which we
    are not yet compatible with), we need to exclude this from the set of
    Python versions for which we are compiling wheels. While we're at it,
    this commit specifies a list of versions instead that we can update as
    we go along.

--- tools/cufinufft/build-wheels.sh
+++ tools/cufinufft/build-wheels.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e -u -x
 
+function get_python_binary {
+    version="$1"
+    echo "/opt/python/$version/bin"
+}
+
 function repair_wheel {
     wheel="$1"
     if ! "${PYBIN}/auditwheel" show "$wheel"; then
@@ -10,13 +15,22 @@ function repair_wheel {
     fi
 }
 
+# Explicitly list Python versions to build for
+PYVERSIONS=(cp36-cp36m \
+            cp37-cp37m \
+            cp38-cp38 \
+            cp39-cp39 \
+            cp310-cp310 \
+            cp311-cp311)
 
 # Compile wheels
-for PYBIN in /opt/python/cp3*/bin; do
+for PYVERSION in ${PYVERSIONS[@]}; do
+    PYBIN=$(get_python_binary ${PYVERSION})
+
     "${PYBIN}/pip" install --upgrade pip
     "${PYBIN}/pip" install -r /io/python/cufinufft/requirements.txt
     "${PYBIN}/pip" install auditwheel pytest
-    "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
+    "${PYBIN}/pip" wheel /io/python/cufinufft --no-deps -w wheelhouse/
 done
 
 
@@ -25,9 +39,10 @@ for whl in wheelhouse/*.whl; do
     repair_wheel "$whl"
 done
 
-
 # Install packages and test
-for PYBIN in /opt/python/cp3*/bin/; do
+for PYVERSION in ${PYVERSIONS[@]}; do
+    PYBIN=$(get_python_binary ${PYVERSION})
+
     "${PYBIN}/pip" install cufinufft -f /io/wheelhouse
     "${PYBIN}/python" -m pytest /io/python/cufinufft/tests
 done
