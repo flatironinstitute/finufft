06a32d934b509902cc59f43d4c4d39f7bdbcf799
Robert Blackwell <rblackwell@flatironinstitute.org>
2023-06-28 15:56:06 -0400

    cuda: remove unnecessary cufinufft_plan_t template specialization
    see #278 post-merge discussion

--- include/cufinufft/types.h
+++ include/cufinufft/types.h
@@ -11,105 +11,7 @@
 
 #define CUFINUFFT_BIGINT int
 
-template <typename T>
-struct cufinufft_plan_t;
-
-template <>
-struct cufinufft_plan_t<float> {
-    cufinufft_opts opts;
-    finufft_spread_opts spopts;
-
-    int type;
-    int dim;
-    CUFINUFFT_BIGINT M;
-    CUFINUFFT_BIGINT nf1;
-    CUFINUFFT_BIGINT nf2;
-    CUFINUFFT_BIGINT nf3;
-    CUFINUFFT_BIGINT ms;
-    CUFINUFFT_BIGINT mt;
-    CUFINUFFT_BIGINT mu;
-    int ntransf;
-    int maxbatchsize;
-    int iflag;
-
-    int totalnumsubprob;
-    int byte_now;
-    float *fwkerhalf1;
-    float *fwkerhalf2;
-    float *fwkerhalf3;
-
-    float *kx;
-    float *ky;
-    float *kz;
-    cuFloatComplex *c;
-    cuFloatComplex *fw;
-    cuFloatComplex *fk;
-
-    // Arrays that used in subprob method
-    int *idxnupts;        // length: #nupts, index of the nupts in the bin-sorted order
-    int *sortidx;         // length: #nupts, order inside the bin the nupt belongs to
-    int *numsubprob;      // length: #bins,  number of subproblems in each bin
-    int *binsize;         // length: #bins, number of nonuniform ponits in each bin
-    int *binstartpts;     // length: #bins, exclusive scan of array binsize
-    int *subprob_to_bin;  // length: #subproblems, the bin the subproblem works on
-    int *subprobstartpts; // length: #bins, exclusive scan of array numsubprob
-
-    // Arrays for 3d (need to sort out)
-    int *numnupts;
-    int *subprob_to_nupts;
-
-    cufftHandle fftplan;
-    cudaStream_t *streams;
-};
-
-template <>
-struct cufinufft_plan_t<double> {
-    cufinufft_opts opts;
-    finufft_spread_opts spopts;
-
-    int type;
-    int dim;
-    CUFINUFFT_BIGINT M;
-    CUFINUFFT_BIGINT nf1;
-    CUFINUFFT_BIGINT nf2;
-    CUFINUFFT_BIGINT nf3;
-    CUFINUFFT_BIGINT ms;
-    CUFINUFFT_BIGINT mt;
-    CUFINUFFT_BIGINT mu;
-    int ntransf;
-    int maxbatchsize;
-    int iflag;
-
-    int totalnumsubprob;
-    int byte_now;
-    double *fwkerhalf1;
-    double *fwkerhalf2;
-    double *fwkerhalf3;
-
-    double *kx;
-    double *ky;
-    double *kz;
-    cuDoubleComplex *c;
-    cuDoubleComplex *fw;
-    cuDoubleComplex *fk;
-
-    // Arrays that used in subprob method
-    int *idxnupts;        // length: #nupts, index of the nupts in the bin-sorted order
-    int *sortidx;         // length: #nupts, order inside the bin the nupt belongs to
-    int *numsubprob;      // length: #bins,  number of subproblems in each bin
-    int *binsize;         // length: #bins, number of nonuniform ponits in each bin
-    int *binstartpts;     // length: #bins, exclusive scan of array binsize
-    int *subprob_to_bin;  // length: #subproblems, the bin the subproblem works on
-    int *subprobstartpts; // length: #bins, exclusive scan of array numsubprob
-
-    // Arrays for 3d (need to sort out)
-    int *numnupts;
-    int *subprob_to_nupts;
-
-    cufftHandle fftplan;
-    cudaStream_t *streams;
-};
-
+// Ugly trick to map a template to a fixed type, here cuda_complex<T>
 template <typename T>
 struct cuda_complex_impl;
 template <>
@@ -123,6 +25,56 @@ struct cuda_complex_impl<double> {
 
 template <typename T>
 using cuda_complex = typename cuda_complex_impl<T>::type;
+
+
+template <typename T>
+struct cufinufft_plan_t {
+    cufinufft_opts opts;
+    finufft_spread_opts spopts;
+
+    int type;
+    int dim;
+    CUFINUFFT_BIGINT M;
+    CUFINUFFT_BIGINT nf1;
+    CUFINUFFT_BIGINT nf2;
+    CUFINUFFT_BIGINT nf3;
+    CUFINUFFT_BIGINT ms;
+    CUFINUFFT_BIGINT mt;
+    CUFINUFFT_BIGINT mu;
+    int ntransf;
+    int maxbatchsize;
+    int iflag;
+
+    int totalnumsubprob;
+    int byte_now;
+    T *fwkerhalf1;
+    T *fwkerhalf2;
+    T *fwkerhalf3;
+
+    T *kx;
+    T *ky;
+    T *kz;
+    cuda_complex<T> *c;
+    cuda_complex<T> *fw;
+    cuda_complex<T> *fk;
+
+    // Arrays that used in subprob method
+    int *idxnupts;        // length: #nupts, index of the nupts in the bin-sorted order
+    int *sortidx;         // length: #nupts, order inside the bin the nupt belongs to
+    int *numsubprob;      // length: #bins,  number of subproblems in each bin
+    int *binsize;         // length: #bins, number of nonuniform ponits in each bin
+    int *binstartpts;     // length: #bins, exclusive scan of array binsize
+    int *subprob_to_bin;  // length: #subproblems, the bin the subproblem works on
+    int *subprobstartpts; // length: #bins, exclusive scan of array numsubprob
+
+    // Arrays for 3d (need to sort out)
+    int *numnupts;
+    int *subprob_to_nupts;
+
+    cufftHandle fftplan;
+    cudaStream_t *streams;
+};
+
 
 template <typename T>
 static cufftType_t cufft_type();
