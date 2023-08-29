db86ea049a9d203df3e2d412ef853c1456d00de0
Joakim Andén <janden@kth.se>
2023-08-29 19:47:23 +0200

    Factor out problem setup and verification in tests
    Similar to the setup in cufinufft tests.

--- python/finufft/test/test_finufft_simple.py
+++ python/finufft/test/test_finufft_simple.py
@@ -3,70 +3,8 @@ import pytest
 import numpy as np
 import finufft
 
-def _get_real_dtype(dtype):
-    return np.array(0, dtype=dtype).real.dtype
+import utils
 
-def _gen_pts(dim, n_pts, dtype):
-    real_dtype = _get_real_dtype(dtype)
-    pts =  2 * np.pi * np.random.rand(dim, n_pts).astype(real_dtype)
-    return pts
-
-def _gen_coefs(n_pts, n_tr, dtype):
-    coefs = np.random.randn(*n_tr, n_pts) + 1j * np.random.randn(*n_tr, n_pts)
-    coefs = coefs.astype(dtype)
-    return coefs
-
-def _gen_sig(n_modes, n_tr, dtype):
-    sig = np.random.randn(*n_tr, *n_modes) + 1j * np.random.randn(*n_tr, *n_modes)
-    sig = sig.astype(dtype)
-    return sig
-
-def _gen_sig_idx(n_modes, n_tr):
-    idx = tuple(np.random.randint(0, n) for n in n_tr + n_modes)
-    return idx
-
-def _gen_coef_ind(n_pts, n_tr):
-    ind = tuple(np.random.randint(0, n) for n in n_tr + (n_pts,))
-    return ind
-
-def _nudft1(pts, coefs, n_modes, idx):
-    dtype = coefs.dtype
-    dim = len(n_modes)
-
-    _idx = (np.array(idx[-dim:]) - np.floor(np.array(n_modes) / 2)).astype(dtype)
-
-    _coefs = coefs[idx[:-dim]]
-
-    sig = np.sum(np.exp(1j * np.sum(_idx[:, np.newaxis] * pts, axis=0)) * _coefs)
-
-    return sig
-
-def _nudft2(pts, sig, ind):
-    dtype = sig.dtype
-    dim = pts.shape[0]
-    n_modes = sig.shape[-dim:]
-
-    grids = [slice(-np.floor(n / 2), np.ceil(n / 2)) for n in n_modes]
-    grid = np.mgrid[grids].astype(dtype)
-
-    pt = pts[:, ind[-1]]
-    pt = pt.reshape((dim,) + dim * (1,))
-
-    _sig = sig[ind[:-1]]
-
-    coef = np.sum(np.exp(-1j * np.sum(pt * grid, axis=0)) * _sig)
-
-    return coef
-
-def _nudft3(source_pts, source_coef, target_pts, ind):
-    target_pt = target_pts[:, ind[-1]]
-    target_pt = target_pt[:, np.newaxis]
-
-    _source_coef = source_coef[ind[:-1]]
-
-    target_coef = np.sum(np.exp(1j * np.sum(target_pt * source_pts, axis=0)) * _source_coef)
-
-    return target_coef
 
 @pytest.mark.parametrize("n_modes", [(7,), (8,), (7, 7), (7, 8), (8, 8), (7, 7, 7), (7, 8, 8), (8, 8, 8)])
 @pytest.mark.parametrize("n_pts", [10, 11])
@@ -75,6 +13,7 @@ def _nudft3(source_pts, source_coef, target_pts, ind):
 @pytest.mark.parametrize("use_out", [False, True])
 def test_nufft1(n_modes, n_pts, n_tr, dtype, use_out):
     dim = len(n_modes)
+    real_dtype = utils._real_dtype(dtype)
 
     funs = {1: finufft.nufft1d1,
             2: finufft.nufft2d1,
@@ -84,9 +23,7 @@ def test_nufft1(n_modes, n_pts, n_tr, dtype, use_out):
 
     np.random.seed(0)
 
-    pts = _gen_pts(dim, n_pts, dtype)
-    coefs = _gen_coefs(n_pts, n_tr, dtype)
-    idx = _gen_sig_idx(n_modes, n_tr)
+    pts, coefs = utils.type1_problem(real_dtype, n_modes, n_pts, n_trans=n_tr)
 
     # See if it can handle square sizes from ints
     if all(n == n_modes[0] for n in n_modes):
@@ -100,10 +37,7 @@ def test_nufft1(n_modes, n_pts, n_tr, dtype, use_out):
         sig = np.empty(n_tr + n_modes, dtype=dtype)
         fun(*pts, coefs, out=sig)
 
-    sig0 = _nudft1(pts, coefs, n_modes, idx)
-
-    assert sig.shape == n_tr + n_modes
-    assert np.isclose(sig[idx], sig0)
+    utils.verify_type1(pts, coefs, sig, 1e-6)
 
 @pytest.mark.parametrize("n_modes", [(7,), (8,), (7, 7), (7, 8), (8, 8), (7, 7, 7), (7, 8, 8), (8, 8, 8)])
 @pytest.mark.parametrize("n_pts", [10, 11])
@@ -112,6 +46,7 @@ def test_nufft1(n_modes, n_pts, n_tr, dtype, use_out):
 @pytest.mark.parametrize("use_out", [False, True])
 def test_nufft2(n_modes, n_pts, n_tr, dtype, use_out):
     dim = len(n_modes)
+    real_dtype = utils._real_dtype(dtype)
 
     funs = {1: finufft.nufft1d2,
             2: finufft.nufft2d2,
@@ -121,9 +56,7 @@ def test_nufft2(n_modes, n_pts, n_tr, dtype, use_out):
 
     np.random.seed(0)
 
-    pts = _gen_pts(dim, n_pts, dtype)
-    sig = _gen_sig(n_modes, n_tr, dtype)
-    ind = _gen_coef_ind(n_pts, n_tr)
+    pts, sig = utils.type2_problem(real_dtype, n_modes, n_pts, n_trans=n_tr)
 
     if not use_out:
         coef = fun(*pts, sig)
@@ -131,18 +64,7 @@ def test_nufft2(n_modes, n_pts, n_tr, dtype, use_out):
         coef = np.empty(n_tr + (n_pts,), dtype=dtype)
         fun(*pts, sig, out=coef)
 
-    coef0 = _nudft2(pts, sig, ind)
-
-    assert coef.shape == n_tr + (n_pts,)
-
-    # NOTE: Tolerance should be tighter for n_tr != 1, but for now, loosen the
-    # tolerance.
-    if n_tr != ():
-        rtol = 1e-4
-    else:
-        rtol = 1e-5
-
-    assert np.isclose(coef[ind], coef0, rtol=rtol)
+    utils.verify_type2(pts, sig, coef, 1e-6)
 
 @pytest.mark.parametrize("dim", [1, 2, 3])
 @pytest.mark.parametrize("n_source_pts", [10, 11])
@@ -151,6 +73,8 @@ def test_nufft2(n_modes, n_pts, n_tr, dtype, use_out):
 @pytest.mark.parametrize("dtype", ["complex64", "complex128"])
 @pytest.mark.parametrize("use_out", [False, True])
 def test_nufft3(dim, n_source_pts, n_target_pts, n_tr, dtype, use_out):
+    real_dtype = utils._real_dtype(dtype)
+
     funs = {1: finufft.nufft1d3,
             2: finufft.nufft2d3,
             3: finufft.nufft3d3}
@@ -159,10 +83,8 @@ def test_nufft3(dim, n_source_pts, n_target_pts, n_tr, dtype, use_out):
 
     np.random.seed(0)
 
-    source_pts = _gen_pts(dim, n_source_pts, dtype)
-    target_pts = _gen_pts(dim, n_target_pts, dtype)
-    source_coefs = _gen_coefs(n_source_pts, n_tr, dtype)
-    ind = _gen_coef_ind(n_target_pts, n_tr)
+    source_pts, source_coefs, target_pts = utils.type3_problem(real_dtype,
+            dim, n_source_pts, n_target_pts, n_trans=n_tr)
 
     if not use_out:
         target_coef = fun(*source_pts, source_coefs, *target_pts)
@@ -170,18 +92,7 @@ def test_nufft3(dim, n_source_pts, n_target_pts, n_tr, dtype, use_out):
         target_coef = np.empty(n_tr + (n_target_pts,), dtype=dtype)
         fun(*source_pts, source_coefs, *target_pts, out=target_coef)
 
-    target_coef0 = _nudft3(source_pts, source_coefs, target_pts, ind)
-
-    assert target_coef.shape == n_tr + (n_target_pts,)
-
-    # NOTE: Tolerance should be tighter for n_tr != 1, but for now, loosen the
-    # tolerance.
-    if n_tr != ():
-        rtol = 1e-3
-    else:
-        rtol = 1e-4
-
-    assert np.isclose(target_coef[ind], target_coef0, rtol=rtol)
+    utils.verify_type3(source_pts, source_coefs, target_pts, target_coef, 1e-6)
 
 def test_errors():
     with pytest.raises(RuntimeError, match="x dtype should be"):
