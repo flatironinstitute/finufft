1592e90920344512e05608379d875973dd846cd5
Joakim Andén <janden@kth.se>
2023-12-27 00:26:23 +0100

    cuda+py: add getting started for other frameworks

---
+++ python/cufinufft/examples/getting_started_torch.py
@@ -0,0 +1,19 @@
+import torch
+
+import cufinufft
+
+# number of nonuniform points
+M = 100000
+
+# grid size
+N = 200000
+
+# generate positions for the nonuniform points and the coefficients
+x_gpu = 2 * torch.pi * torch.rand(size=(M,)).cuda()
+c_gpu = (torch.randn(size=(M,)) + 1J * torch.randn(size=(M,))).cuda()
+
+# compute the transform
+f_gpu = cufinufft.nufft1d1(x_gpu, c_gpu, (N,))
+
+# move results off the GPU
+f = f_gpu.cpu()
