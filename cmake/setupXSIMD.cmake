6e834c87c1ab908d79f27a1a2062ac73f074e347
Marco Barbone <mbarbone@ccmlin073.flatironinstitute.org>
2024-05-29 14:43:47 -0400

    added cmake file to find xsimd

---
+++ cmake/setupXSIMD.cmake
@@ -0,0 +1,19 @@
+CPMAddPackage(
+        NAME findxtl
+        GIT_REPOSITORY "https://github.com/xtensor-stack/xtl.git"
+        GIT_TAG ${XTL_VERSION}
+        EXCLUDE_FROM_ALL YES
+        GIT_SHALLOW YES
+        OPTIONS "XTL_DISABLE_EXCEPTIONS YES"
+)
+
+CPMAddPackage(
+        NAME findxsimd
+        GIT_REPOSITORY "https://github.com/xtensor-stack/xsimd.git"
+        GIT_TAG ${XSIMD_VERSION}
+        EXCLUDE_FROM_ALL YES
+        GIT_SHALLOW YES
+        OPTIONS
+            "XSIMD_SKIP_INSTALL YES"
+            "XSIMD_ENABLE_XTL_COMPLEX YES"
+)
