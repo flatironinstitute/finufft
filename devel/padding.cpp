1d638d6a31cad1f3d617e9c3ff62a784b4a51b3f
Marco Barbone <mbarbone@ccmlin073.flatironinstitute.org>
2024-05-31 18:18:49 -0400

    started building the sequence

--- devel/padding.cpp
+++ devel/padding.cpp
@@ -24,6 +24,9 @@ using BestSIMD = typename decltype(BestSIMDHelper<T, N, xsimd::batch<T>::size>()
 template<class T, uint16_t N = 1>
 static constexpr uint16_t min_batch_size();
 
+template<class T, uint16_t N = min_batch_size<T>()>
+constexpr uint16_t max_batch_size();
+
 template<class T, uint16_t N>
 static constexpr auto find_optimal_batch_size();
 
@@ -32,17 +35,6 @@ static constexpr auto find_optimal_batch_size();
 // improper use will cause the compiler to either throw an error on the recursion depth
 // or on older ones... "compiler internal error please report"
 // you have been warned.
-//template<class T, uint8_t N, typename = void>
-//struct GetPaddedSIMDSize {
-//  static constexpr uint8_t value = GetPaddedSIMDSize<T, N + 1>::value;
-//};
-//
-//
-//template<class T, uint8_t N>
-//struct GetPaddedSIMDSize<T, N, std::enable_if_t<!std::is_void<BestSIMD_t<T, N>>::value>> {
-//static constexpr uint8_t value = BestSIMD_t<T, N>::size;
-//};
-
 
 template<class T, uint16_t N, uint16_t K>
 static constexpr auto BestSIMDHelper() {
@@ -57,6 +49,15 @@ template<class T, uint16_t N>
 constexpr uint16_t min_batch_size() {
   if constexpr (std::is_void_v<xsimd::make_sized_batch_t<T, N>>) {
     return min_batch_size<T, N*2>();
+  } else {
+    return N;
+  }
+};
+
+template<class T, uint16_t N>
+constexpr uint16_t max_batch_size() {
+  if constexpr (!std::is_void_v<xsimd::make_sized_batch_t<T, N*2>>) {
+    return max_batch_size<T, N*2>();
   } else {
     return N;
   }
@@ -106,7 +107,77 @@ static uint16_t get_padding(uint16_t ns) {
   return get_padding_helper<T, 32>(ns);
 }
 
+template<class T, uint16_t N>
+std::vector<uint16_t> mixed_vectors() {
+  auto min_batch = min_batch_size<T>();
+  auto max_batch = max_batch_size<T>();
+  std::vector<uint16_t> batch_sizes{1};
+  std::vector<uint16_t> chosen_batch_sizes(N+1, 0);
+  for (uint16_t i = min_batch; i <= max_batch; i *= 2) {
+    batch_sizes.push_back(i);
+  }
+  std::vector<uint16_t> dp(N+1, N+1);  // Initialize the dp table
+  dp[0] = 0;  // 0 amount requires 0 coins
+
+  for (uint16_t i = 0; i <= N; ++i) {
+    for (const auto batch_size : batch_sizes) {
+      if (batch_size <= i && dp[i - batch_size] + 1 < dp[i]) {
+        dp[i] = dp[i - batch_size] + 1;
+        chosen_batch_sizes[i] = batch_size;
+      }
+    }
+  }
+
+  // Build the sequence of coins that fit in N
+  std::vector<uint16_t> sequence;
+  for (int i = N; i > 0; i -= chosen_batch_sizes[i]) {
+    sequence.push_back(chosen_batch_sizes[i]);
+  }
+  return sequence;
+}
+
+template<class T>
+auto& print(std::vector<T> arg) {
+  T sum = 0;
+  for (const auto &elem : arg) {
+    std::cout << elem << " ";
+    sum += elem;
+  }
+  std::cout << "sum is " << sum;
+  return std::cout;
+}
+
 int main(int argc, char *argv[]) {
+  std::cout << "sequence for 16 single precision is ";
+  print(mixed_vectors<float, 16>()) << std::endl;
+  std::cout << "sequence for 16 double precision is ";
+  print(mixed_vectors<double, 16>()) << std::endl;
+
+  std::cout << "sequence for 17 single precision is ";
+  print(mixed_vectors<float, 17>()) << std::endl;
+  std::cout << "sequence for 17 double precision is ";
+  print(mixed_vectors<double, 17>()) << std::endl;
+
+  std::cout << "sequence for 23 single precision is ";
+  print(mixed_vectors<float, 23>()) << std::endl;
+  std::cout << "sequence for 23 double precision is ";
+  print(mixed_vectors<double, 23>()) << std::endl;
+
+  std::cout << "sequence for 18 single precision is ";
+  print(mixed_vectors<float, 18>()) << std::endl;
+  std::cout << "sequence for 18 double precision is ";
+  print(mixed_vectors<double, 18>()) << std::endl;
+
+
+  std::cout << "sequence for 31 single precision is ";
+  print(mixed_vectors<float, 31>()) << std::endl;
+  std::cout << "sequence for 31 double precision is ";
+  print(mixed_vectors<double, 31>()) << std::endl;
+
+  std::cout << "Min batch size for single precision is " << uint64_t(min_batch_size<float>()) << std::endl;
+  std::cout << "Max batch size for single precision is " << uint64_t(max_batch_size<float>()) << std::endl;
+  std::cout << "Min batch size for double precision is " << uint64_t(min_batch_size<double>()) << std::endl;
+  std::cout << "Max batch size for double precision is " << uint64_t(max_batch_size<double>()) << std::endl;
 
   std::cout << "Best SIMD single precision" << std::endl;
   std::cout << "SIMD for " <<  4 << " is " << uint64_t(BestSIMD<float,  4>::size) << std::endl;
