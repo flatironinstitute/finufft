0ff245a1df9c5abc1309e234762bf2477fdbf9f3
Martin Reinecke <martin@mpa-garching.mpg.de>
2024-06-22 14:34:28 +0200

    almost working; some tests don't compile yet

--- cmake/setupDUCC.cmake
+++ cmake/setupDUCC.cmake
@@ -6,8 +6,8 @@ CPMAddPackage(
 )
 
 if(ducc0_ADDED)
-    add_library(ducc0 INTERFACE)
-    target_include_directories(ducc0 INTERFACE ${ducc0_SOURCE_DIR}/src/)
+    add_library(ducc0 OBJECT ${ducc0_SOURCE_DIR}/src/ducc0/infra/string_utils.cc ${ducc0_SOURCE_DIR}/src/ducc0/infra/threading.cc ${ducc0_SOURCE_DIR}/src/ducc0/infra/mav.cc ${ducc0_SOURCE_DIR}/src/ducc0/math/gridding_kernel.cc ${ducc0_SOURCE_DIR}/src/ducc0/math/gl_integrator.cc)
+    target_include_directories(ducc0 PUBLIC ${ducc0_SOURCE_DIR}/src/)
 endif ()
 
 list(APPEND FINUFFT_FFTW_LIBRARIES ducc0)
