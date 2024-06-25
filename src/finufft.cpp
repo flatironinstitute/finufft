601468ac62b3b0fb4a74e24596cde8a2ef8ab41a
Martin Reinecke <martin@mpa-garching.mpg.de>
2024-06-25 09:49:02 +0200

    be more careful about the number of threads to use

--- src/finufft.cpp
+++ src/finufft.cpp
@@ -1141,6 +1141,7 @@ int FINUFFT_EXECUTE(FINUFFT_PLAN p, CPX *cj, CPX *fk) {
       timer.restart();
 #ifdef FINUFFT_USE_DUCC0
       {
+        size_t nthreads = min<size_t>(MY_OMP_GET_MAX_THREADS(), p->opts.nthreads);
         int *ns = GRIDSIZE_FOR_FFT(p);
         vector<size_t> arrdims, axes;
         arrdims.push_back(size_t(p->batchSize));
@@ -1156,10 +1157,10 @@ int FINUFFT_EXECUTE(FINUFFT_PLAN p, CPX *cj, CPX *fk) {
         }
         ducc0::vfmav<CPX> data(fwBatch, arrdims);
         if (p->dim == 1)        // 1D: no chance for FFT shortcuts
-          ducc0::c2c(data, data, axes, p->fftSign < 0, FLT(1), p->opts.nthreads);
+          ducc0::c2c(data, data, axes, p->fftSign < 0, FLT(1), nthreads);
         else if (p->dim == 2) { // 2D: do partial FFTs
           if (p->ms < 2)        // something is weird, do standard FFT
-            ducc0::c2c(data, data, axes, p->fftSign < 0, FLT(1), p->opts.nthreads);
+            ducc0::c2c(data, data, axes, p->fftSign < 0, FLT(1), nthreads);
           else {
             size_t y_lo = size_t((p->ms + 1) / 2);
             size_t y_hi = size_t(ns[1] - p->ms / 2);
@@ -1167,21 +1168,21 @@ int FINUFFT_EXECUTE(FINUFFT_PLAN p, CPX *cj, CPX *fk) {
             auto sub2   = ducc0::subarray(data, {{}, {}, {y_hi, ducc0::MAXIDX}});
             if (p->type == 1) { // spreading, not all parts of the output array are needed
               // do axis 2 in full
-              ducc0::c2c(data, data, {2}, p->fftSign < 0, FLT(1), p->opts.nthreads);
+              ducc0::c2c(data, data, {2}, p->fftSign < 0, FLT(1), nthreads);
               // do only parts of axis 1
-              ducc0::c2c(sub1, sub1, {1}, p->fftSign < 0, FLT(1), p->opts.nthreads);
-              ducc0::c2c(sub2, sub2, {1}, p->fftSign < 0, FLT(1), p->opts.nthreads);
+              ducc0::c2c(sub1, sub1, {1}, p->fftSign < 0, FLT(1), nthreads);
+              ducc0::c2c(sub2, sub2, {1}, p->fftSign < 0, FLT(1), nthreads);
             } else { // interpolation, parts of the input array are zero
               // do only parts of axis 1
-              ducc0::c2c(sub1, sub1, {1}, p->fftSign < 0, FLT(1), p->opts.nthreads);
-              ducc0::c2c(sub2, sub2, {1}, p->fftSign < 0, FLT(1), p->opts.nthreads);
+              ducc0::c2c(sub1, sub1, {1}, p->fftSign < 0, FLT(1), nthreads);
+              ducc0::c2c(sub2, sub2, {1}, p->fftSign < 0, FLT(1), nthreads);
               // do axis 2 in full
-              ducc0::c2c(data, data, {2}, p->fftSign < 0, FLT(1), p->opts.nthreads);
+              ducc0::c2c(data, data, {2}, p->fftSign < 0, FLT(1), nthreads);
             }
           }
         } else {                          // 3D
           if ((p->ms < 2) || (p->mt < 2)) // something is weird, do standard FFT
-            ducc0::c2c(data, data, axes, p->fftSign < 0, FLT(1), p->opts.nthreads);
+            ducc0::c2c(data, data, axes, p->fftSign < 0, FLT(1), nthreads);
           else {
             size_t z_lo = size_t((p->ms + 1) / 2);
             size_t z_hi = size_t(ns[2] - p->ms / 2);
@@ -1195,26 +1196,26 @@ int FINUFFT_EXECUTE(FINUFFT_PLAN p, CPX *cj, CPX *fk) {
             auto sub6   = ducc0::subarray(sub2, {{}, {}, {y_hi, ducc0::MAXIDX}, {}});
             if (p->type == 1) { // spreading, not all parts of the output array are needed
               // do axis 3 in full
-              ducc0::c2c(data, data, {3}, p->fftSign < 0, FLT(1), p->opts.nthreads);
+              ducc0::c2c(data, data, {3}, p->fftSign < 0, FLT(1), nthreads);
               // do only parts of axis 2
-              ducc0::c2c(sub1, sub1, {2}, p->fftSign < 0, FLT(1), p->opts.nthreads);
-              ducc0::c2c(sub2, sub2, {2}, p->fftSign < 0, FLT(1), p->opts.nthreads);
+              ducc0::c2c(sub1, sub1, {2}, p->fftSign < 0, FLT(1), nthreads);
+              ducc0::c2c(sub2, sub2, {2}, p->fftSign < 0, FLT(1), nthreads);
               // do even smaller parts of axis 1
-              ducc0::c2c(sub3, sub3, {1}, p->fftSign < 0, FLT(1), p->opts.nthreads);
-              ducc0::c2c(sub4, sub4, {1}, p->fftSign < 0, FLT(1), p->opts.nthreads);
-              ducc0::c2c(sub5, sub5, {1}, p->fftSign < 0, FLT(1), p->opts.nthreads);
-              ducc0::c2c(sub6, sub6, {1}, p->fftSign < 0, FLT(1), p->opts.nthreads);
+              ducc0::c2c(sub3, sub3, {1}, p->fftSign < 0, FLT(1), nthreads);
+              ducc0::c2c(sub4, sub4, {1}, p->fftSign < 0, FLT(1), nthreads);
+              ducc0::c2c(sub5, sub5, {1}, p->fftSign < 0, FLT(1), nthreads);
+              ducc0::c2c(sub6, sub6, {1}, p->fftSign < 0, FLT(1), nthreads);
             } else { // interpolation, parts of the input array are zero
               // do even smaller parts of axis 1
-              ducc0::c2c(sub3, sub3, {1}, p->fftSign < 0, FLT(1), p->opts.nthreads);
-              ducc0::c2c(sub4, sub4, {1}, p->fftSign < 0, FLT(1), p->opts.nthreads);
-              ducc0::c2c(sub5, sub5, {1}, p->fftSign < 0, FLT(1), p->opts.nthreads);
-              ducc0::c2c(sub6, sub6, {1}, p->fftSign < 0, FLT(1), p->opts.nthreads);
+              ducc0::c2c(sub3, sub3, {1}, p->fftSign < 0, FLT(1), nthreads);
+              ducc0::c2c(sub4, sub4, {1}, p->fftSign < 0, FLT(1), nthreads);
+              ducc0::c2c(sub5, sub5, {1}, p->fftSign < 0, FLT(1), nthreads);
+              ducc0::c2c(sub6, sub6, {1}, p->fftSign < 0, FLT(1), nthreads);
               // do only parts of axis 2
-              ducc0::c2c(sub1, sub1, {2}, p->fftSign < 0, FLT(1), p->opts.nthreads);
-              ducc0::c2c(sub2, sub2, {2}, p->fftSign < 0, FLT(1), p->opts.nthreads);
+              ducc0::c2c(sub1, sub1, {2}, p->fftSign < 0, FLT(1), nthreads);
+              ducc0::c2c(sub2, sub2, {2}, p->fftSign < 0, FLT(1), nthreads);
               // do axis 3 in full
-              ducc0::c2c(data, data, {3}, p->fftSign < 0, FLT(1), p->opts.nthreads);
+              ducc0::c2c(data, data, {3}, p->fftSign < 0, FLT(1), nthreads);
             }
           }
         }
