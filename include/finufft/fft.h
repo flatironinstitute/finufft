9f86e46c8de5b287d21994a7c4242b2c83f8e815
Martin Reinecke <martin@mpa-garching.mpg.de>
2024-07-03 21:42:33 +0200

    move central FFT part to separate file

--- include/finufft/fft.h
+++ include/finufft/fft.h
@@ -1,6 +1,7 @@
 #ifndef FINUFFT_INCLUDE_FINUFFT_FFT_H
 #define FINUFFT_INCLUDE_FINUFFT_FFT_H
 
+#include <finufft/defs.h>
 #ifdef FINUFFT_USE_DUCC0
 #include "ducc0/fft/fftnd_impl.h"
 #define FFTW_FORGET_WISDOM()   // temporary hack since some tests call this unconditionally
@@ -10,5 +11,8 @@
 #else
 #include "fftw_defs.h"
 #endif
+
+int *gridsize_for_fft(FINUFFT_PLAN p, CPX *fwBatch);
+void do_fft (FINUFFT_PLAN p, CPX *fwBatch);
 
 #endif // FINUFFT_INCLUDE_FINUFFT_FFT_H
--- src/finufft.cpp
+++ src/finufft.cpp
@@ -489,34 +489,6 @@ int deconvolveBatch(int batchSize, FINUFFT_PLAN p, CPX *fwBatch, CPX *fkBatch)
   return 0;
 }
 
-// since this func is local only, we macro its name here...
-#ifdef SINGLE
-#define GRIDSIZE_FOR_FFT gridsize_for_fftf
-#else
-#define GRIDSIZE_FOR_FFT gridsize_for_fft
-#endif
-
-int *GRIDSIZE_FOR_FFT(FINUFFT_PLAN p) {
-  // local helper func returns a new int array of length dim, extracted from
-  // the finufft plan, that fftw_plan_many_dft needs as its 2nd argument.
-  int *nf;
-  if (p->dim == 1) {
-    nf    = new int[1];
-    nf[0] = (int)p->nf1;
-  } else if (p->dim == 2) {
-    nf    = new int[2];
-    nf[0] = (int)p->nf2;
-    nf[1] = (int)p->nf1;
-  } // fftw enforced row major ordering, ie dims are backwards ordered
-  else {
-    nf    = new int[3];
-    nf[0] = (int)p->nf3;
-    nf[1] = (int)p->nf2;
-    nf[2] = (int)p->nf1;
-  }
-  return nf;
-}
-
 } // namespace common
 } // namespace finufft
 
@@ -773,7 +745,7 @@ int FINUFFT_MAKEPLAN(int type, int dim, BIGINT *n_modes, int iflag, int ntrans, 
     }
 
     timer.restart(); // plan the FFTW
-    int *ns = GRIDSIZE_FOR_FFT(p);
+    int *ns = gridsize_for_fft(p);
     // fftw_plan_many_dft args: rank, gridsize/dim, howmany, in, inembed, istride,
     // idist, ot, onembed, ostride, odist, sign, flags
     {
@@ -1137,91 +1109,7 @@ int FINUFFT_EXECUTE(FINUFFT_PLAN p, CPX *cj, CPX *fk) {
 
       // STEP 2: call the FFT on this batch
       timer.restart();
-#ifdef FINUFFT_USE_DUCC0
-      {
-        size_t nthreads = min<size_t>(MY_OMP_GET_MAX_THREADS(), p->opts.nthreads);
-        int *ns = GRIDSIZE_FOR_FFT(p);
-        vector<size_t> arrdims, axes;
-        arrdims.push_back(size_t(p->batchSize));
-        arrdims.push_back(size_t(ns[0]));
-        axes.push_back(1);
-        if (p->dim >= 2) {
-          arrdims.push_back(size_t(ns[1]));
-          axes.push_back(2);
-        }
-        if (p->dim >= 3) {
-          arrdims.push_back(size_t(ns[2]));
-          axes.push_back(3);
-        }
-        ducc0::vfmav<CPX> data(fwBatch, arrdims);
-        if (p->dim == 1)        // 1D: no chance for FFT shortcuts
-          ducc0::c2c(data, data, axes, p->fftSign < 0, FLT(1), nthreads);
-        else if (p->dim == 2) { // 2D: do partial FFTs
-          if (p->ms < 2)        // something is weird, do standard FFT
-            ducc0::c2c(data, data, axes, p->fftSign < 0, FLT(1), nthreads);
-          else {
-            size_t y_lo = size_t((p->ms + 1) / 2);
-            size_t y_hi = size_t(ns[1] - p->ms / 2);
-            auto sub1   = ducc0::subarray(data, {{}, {}, {0, y_lo}});
-            auto sub2   = ducc0::subarray(data, {{}, {}, {y_hi, ducc0::MAXIDX}});
-            if (p->type == 1) { // spreading, not all parts of the output array are needed
-              // do axis 2 in full
-              ducc0::c2c(data, data, {2}, p->fftSign < 0, FLT(1), nthreads);
-              // do only parts of axis 1
-              ducc0::c2c(sub1, sub1, {1}, p->fftSign < 0, FLT(1), nthreads);
-              ducc0::c2c(sub2, sub2, {1}, p->fftSign < 0, FLT(1), nthreads);
-            } else { // interpolation, parts of the input array are zero
-              // do only parts of axis 1
-              ducc0::c2c(sub1, sub1, {1}, p->fftSign < 0, FLT(1), nthreads);
-              ducc0::c2c(sub2, sub2, {1}, p->fftSign < 0, FLT(1), nthreads);
-              // do axis 2 in full
-              ducc0::c2c(data, data, {2}, p->fftSign < 0, FLT(1), nthreads);
-            }
-          }
-        } else {                          // 3D
-          if ((p->ms < 2) || (p->mt < 2)) // something is weird, do standard FFT
-            ducc0::c2c(data, data, axes, p->fftSign < 0, FLT(1), nthreads);
-          else {
-            size_t z_lo = size_t((p->ms + 1) / 2);
-            size_t z_hi = size_t(ns[2] - p->ms / 2);
-            size_t y_lo = size_t((p->mt + 1) / 2);
-            size_t y_hi = size_t(ns[1] - p->mt / 2);
-            auto sub1   = ducc0::subarray(data, {{}, {}, {}, {0, z_lo}});
-            auto sub2   = ducc0::subarray(data, {{}, {}, {}, {z_hi, ducc0::MAXIDX}});
-            auto sub3   = ducc0::subarray(sub1, {{}, {}, {0, y_lo}, {}});
-            auto sub4   = ducc0::subarray(sub1, {{}, {}, {y_hi, ducc0::MAXIDX}, {}});
-            auto sub5   = ducc0::subarray(sub2, {{}, {}, {0, y_lo}, {}});
-            auto sub6   = ducc0::subarray(sub2, {{}, {}, {y_hi, ducc0::MAXIDX}, {}});
-            if (p->type == 1) { // spreading, not all parts of the output array are needed
-              // do axis 3 in full
-              ducc0::c2c(data, data, {3}, p->fftSign < 0, FLT(1), nthreads);
-              // do only parts of axis 2
-              ducc0::c2c(sub1, sub1, {2}, p->fftSign < 0, FLT(1), nthreads);
-              ducc0::c2c(sub2, sub2, {2}, p->fftSign < 0, FLT(1), nthreads);
-              // do even smaller parts of axis 1
-              ducc0::c2c(sub3, sub3, {1}, p->fftSign < 0, FLT(1), nthreads);
-              ducc0::c2c(sub4, sub4, {1}, p->fftSign < 0, FLT(1), nthreads);
-              ducc0::c2c(sub5, sub5, {1}, p->fftSign < 0, FLT(1), nthreads);
-              ducc0::c2c(sub6, sub6, {1}, p->fftSign < 0, FLT(1), nthreads);
-            } else { // interpolation, parts of the input array are zero
-              // do even smaller parts of axis 1
-              ducc0::c2c(sub3, sub3, {1}, p->fftSign < 0, FLT(1), nthreads);
-              ducc0::c2c(sub4, sub4, {1}, p->fftSign < 0, FLT(1), nthreads);
-              ducc0::c2c(sub5, sub5, {1}, p->fftSign < 0, FLT(1), nthreads);
-              ducc0::c2c(sub6, sub6, {1}, p->fftSign < 0, FLT(1), nthreads);
-              // do only parts of axis 2
-              ducc0::c2c(sub1, sub1, {2}, p->fftSign < 0, FLT(1), nthreads);
-              ducc0::c2c(sub2, sub2, {2}, p->fftSign < 0, FLT(1), nthreads);
-              // do axis 3 in full
-              ducc0::c2c(data, data, {3}, p->fftSign < 0, FLT(1), nthreads);
-            }
-          }
-        }
-        delete[] ns;
-      }
-#else
-      FFTW_EX(p->fftwPlan); // if thisBatchSize<batchSize it wastes some flops
-#endif
+      do_fft (p, fwBatch);
       t_fft += timer.elapsedsec();
       if (p->opts.debug > 1) printf("\tFFT exec:\t\t%.3g s\n", timer.elapsedsec());
 
