179b75b21004629b5c007232dcce0d0847978f24
Marco Barbone <mbarbone@flatironinstitute.org>
2024-07-18 11:44:52 -0400

    using matrix in cmake

---
+++ .github/workflows/generate_cmake_matrix.py
@@ -0,0 +1,40 @@
+import json
+
+matrix = {
+    "include": []
+}
+
+python_versions = ["3.8", "3.11"]
+build_type = ["Release", "Debug"]
+static_linking = ["On", "Off"]
+combinations = {
+    "ubuntu-22.04": {
+        "compiler": ["llvm", "gcc"],
+        "arch_flags": ["-march=native", "-march=x86-64"]
+    },
+    "windows-2022": {
+        "compiler": ["msvc", "llvm"],
+        "arch_flags": ["/arch:AVX2", "/arch:SSE2"]
+    },
+    "macos-13": {
+        "compiler": ["llvm", "gcc-14"],
+        "arch_flags": ["-march=native", "-march=x86-64"]
+    }
+}
+
+for platform in combinations.keys():
+    for python_version in python_versions:
+        for compiler in combinations[platform]["compiler"]:
+            for arch_flag in combinations[platform]["arch_flags"]:
+                for linking in static_linking:
+                    for build in build_type:
+                        matrix["include"].append({
+                            "os": platform,
+                            "python-version": python_version,
+                            "compiler": compiler,
+                            "arch_flags": arch_flag,
+                            "finufft_static_linking": linking,
+                            "build_type": build
+                        })
+json_str = json.dumps(matrix, ensure_ascii=False)
+print(json_str)
