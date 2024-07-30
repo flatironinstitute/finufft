b43247a82536b03b76e35047795d98d35f7bd86a
Marco Barbone <mbarbone@flatironinstitute.org>
2024-07-30 14:04:47 -0400

    updated docs

--- cmake/setupDUCC.cmake
+++ cmake/setupDUCC.cmake
@@ -17,10 +17,15 @@ if(ducc0_ADDED)
     ${ducc0_SOURCE_DIR}/src/ducc0/math/gridding_kernel.cc
     ${ducc0_SOURCE_DIR}/src/ducc0/math/gl_integrator.cc)
   target_include_directories(ducc0 PUBLIC ${ducc0_SOURCE_DIR}/src/)
-  target_compile_options(ducc0 PRIVATE -ffast-math)
-  target_compile_features(
-    ducc0 PRIVATE cxx_std_17) # private because we do not want to propagate this
-                              # requirement
+  target_compile_options(
+    ducc0 PRIVATE $<$<CONFIG:Release,RelWithDebInfo>:${FINUFFT_ARCH_FLAGS}>)
+  target_compile_options(
+    ducc0 PRIVATE $<$<CONFIG:Release>:${FINUFFT_CXX_FLAGS_RELEASE}>)
+  target_compile_options(
+    ducc0
+    PRIVATE $<$<CONFIG:RelWithDebInfo>:${FINUFFT_CXX_FLAGS_RELWITHDEBINFO}>)
+  target_compile_features(ducc0 PRIVATE cxx_std_17)
+  # private because we do not want to propagate this requirement
   set_target_properties(
     ducc0
     PROPERTIES MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>"
