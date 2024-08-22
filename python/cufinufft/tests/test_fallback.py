b5658ce695e37bdae5f55b3ca01c802ae06face0
Joakim Andén <janden@kth.se>
2024-08-22 21:34:46 +0200

    py: add tests for fallback library loading
    These tests checks that the fallback mechanism is working properly. In
    other words, if there is no bundled library, it should look in the
    system path (e.g. `LD_LIBRARY_PATH` on Unix systems) and try to load it
    from there.

---
+++ python/cufinufft/tests/test_fallback.py
@@ -0,0 +1,24 @@
+import pytest
+
+import numpy as np
+from ctypes.util import find_library
+
+
+# Check to make sure the fallback mechanism works if there is no bundled
+# dynamic library.
+def test_fallback(mocker):
+    def fake_load_library(lib_name, path):
+        if lib_name in ["libcufinufft", "cufinufft"]:
+            raise OSError()
+        else:
+            return np.ctypeslib.load_library(lib_name, path)
+
+    # Block out the bundled library.
+    mocker.patch("numpy.ctypeslib.load_library", fake_load_library)
+
+    # Make sure an error is raised if no system library is found.
+    if find_library("cufinufft") is None:
+        with pytest.raises(ImportError, match="suitable cufinufft"):
+            import cufinufft
+    else:
+        import cufinufft
