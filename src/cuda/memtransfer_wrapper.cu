ffd9b9f77237007240536fb3e511a8d304320d68
Chaithya G R <chaithyagr@gmail.com>
2024-09-24 10:07:08 +0200

    Minor memory fix

--- src/cuda/memtransfer_wrapper.cu
+++ src/cuda/memtransfer_wrapper.cu
@@ -434,7 +434,9 @@ void freegpumemory(cufinufft_plan_t<T> *d_plan)
   utils::WithCudaDevice device_swapper(d_plan->opts.gpu_device_id);
   // Fixes a crash whewre the plan itself is deleted before the stream
   const auto stream = d_plan->stream;
-  CUDA_FREE_AND_NULL(d_plan->fw, stream, d_plan->supports_pools);
+  // Dont clear fw if spreadinterponly for type 1 and 2 as fw belongs to original program (it is d_fk)
+  if(!d_plan->opts.gpu_spreadinterponly || d_plan->type == 3)
+    CUDA_FREE_AND_NULL(d_plan->fw, stream, d_plan->supports_pools);
   CUDA_FREE_AND_NULL(d_plan->fwkerhalf1, stream, d_plan->supports_pools);
   CUDA_FREE_AND_NULL(d_plan->fwkerhalf2, stream, d_plan->supports_pools);
   CUDA_FREE_AND_NULL(d_plan->fwkerhalf3, stream, d_plan->supports_pools);
