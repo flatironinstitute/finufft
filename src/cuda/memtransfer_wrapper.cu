d6c646e1f39ae20a43407999ef6bc6199b3e3580
Chaithya G R <chaithyagr@gmail.com>
2024-09-24 09:46:10 +0200

    revert fixes, problem was elsewhere

--- src/cuda/memtransfer_wrapper.cu
+++ src/cuda/memtransfer_wrapper.cu
@@ -426,12 +426,10 @@ void freegpumemory(cufinufft_plan_t<T> *d_plan)
   // Fixes a crash whewre the plan itself is deleted before the stream
   const auto stream = d_plan->stream;
 
-  if (!d_plan->opts.gpu_spreadinterponly) {
-    CUDA_FREE_AND_NULL(d_plan->fw, stream, d_plan->supports_pools);
-    CUDA_FREE_AND_NULL(d_plan->fwkerhalf1, stream, d_plan->supports_pools);
-    CUDA_FREE_AND_NULL(d_plan->fwkerhalf2, stream, d_plan->supports_pools);
-    CUDA_FREE_AND_NULL(d_plan->fwkerhalf3, stream, d_plan->supports_pools);
-  }
+  CUDA_FREE_AND_NULL(d_plan->fw, stream, d_plan->supports_pools);
+  CUDA_FREE_AND_NULL(d_plan->fwkerhalf1, stream, d_plan->supports_pools);
+  CUDA_FREE_AND_NULL(d_plan->fwkerhalf2, stream, d_plan->supports_pools);
+  CUDA_FREE_AND_NULL(d_plan->fwkerhalf3, stream, d_plan->supports_pools);
 
   CUDA_FREE_AND_NULL(d_plan->idxnupts, stream, d_plan->supports_pools);
   CUDA_FREE_AND_NULL(d_plan->sortidx, stream, d_plan->supports_pools);
