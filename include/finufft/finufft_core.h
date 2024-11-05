33d80c3ee39f5d2a801914fe8d5068d1aa5298ad
ahbarnett <alex.h.barnett@gmail.com>
2024-11-05 14:21:48 -0500

    bump to v2.3.1 throughout, as per devnotes

--- include/finufft/finufft_core.h
+++ include/finufft/finufft_core.h
@@ -71,7 +71,7 @@ using UBIGINT = uint64_t;
 // ------------- Library-wide algorithm parameter settings ----------------
 
 // Library version (is a string)
-#define FINUFFT_VER "2.3.0"
+#define FINUFFT_VER "2.3.1"
 
 // Smallest possible kernel spread width per dimension, in fine grid points
 // (used only in spreadinterp.cpp)
