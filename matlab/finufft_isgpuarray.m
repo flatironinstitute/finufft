60f56f3619e799615dd2890774d782f85e6d700e
Libin Lu <llu@flatironinstitute.org>
2025-02-13 22:34:47 -0500

    fix finufft_isgpuarray function

--- matlab/finufft_isgpuarray.m
+++ matlab/finufft_isgpuarray.m
@@ -2,9 +2,9 @@ function is_gpuarray = finufft_isgpuarray(input)
     % Check if the isgpuarray function is available
     if exist('isgpuarray') == 0
       % return 0, since no parallel computing toolbox, can not use gpuarray
-      return logical(0);
+      is_gpuarray = logical(0);
     else
       % call the parallel computing toolbox isgpuarray function
-      return isgpuarray(input);
+      is_gpuarray = isgpuarray(input);
     end
 end
