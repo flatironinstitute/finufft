0684a55d7c3013fad4a2a96e21008b3f6dea461f
ahbarnett <ahb@math.dartmouth.edu>
2025-03-31 23:26:39 -0400

    matlab mexcuda doc generation for .rst

--- docs/genmatlabhelp.sh
+++ docs/genmatlabhelp.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-# this extracts and concatenates MATLAB documentation blocks from the .m files.
+# This extracts and concatenates MATLAB documentation blocks from the .m files.
 # Three steps: 1) keep comment lines beginning with %, 2) remove the first
-# char of each line, then 3) remove the header (until the first line of ----).
-# This relies on having such a -------- line in the .mw file.
-# Barnett 11/2/17, changed output name 7/24/20.
+# char of each line.
+# Barnett 11/2/17, changed output name 7/24/20. Added GPU 3/31/25.
 
-# The output is a text file...
+# CPU: The output is a text file...
 OUT=matlabhelp.doc
 
 # zero the size...
@@ -18,3 +17,20 @@ do
     sed -n '/^%/p' $i | sed 's/^.//' >> $OUT
     printf "\n" >> $OUT
 done
+
+
+# ---------------------------------
+# now GPU: output is a text file...
+OUT=matlabgpuhelp.doc
+
+# zero the size...
+> $OUT
+
+# dump the matlab comment blocks
+for i in ../matlab/@gpuArray/finufft?d?.m ../matlab/@gpuArray/cufinufft_plan.m
+do
+    printf "::\n\n" >> $OUT
+    sed -n '/^%/p' $i | sed 's/^.//' >> $OUT
+    printf "\n" >> $OUT
+done
+
