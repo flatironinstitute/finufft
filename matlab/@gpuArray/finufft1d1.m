bcbea394dd7a078c938da0894547ef7a9eee5533
ahbarnett <ahb@math.dartmouth.edu>
2025-04-01 22:31:09 -0400

    mexcuda simplest possible overloads in @gpuArray

--- matlab/@gpuArray/finufft1d1.m
+++ matlab/@gpuArray/finufft1d1.m
@@ -1,5 +1,5 @@
-% FINUFFT1D1   GPU 1D complex nonuniform FFT of type 1 (nonuniform to uniform).
+% FINUFFT1D1   GPU 1D complex nonuniform FFT, type 1 (nonuniform to uniform).
 %
-% See also CUFINUFFT1D1
-function f = finufft1d1(x,c,isign,eps,ms,o)
-f = cufinufft1d1(x,c,isign,eps,ms,o)
+% See CUFINUFFT1D1
+function f = finufft1d1(varargin)
+f = cufinufft1d1(varargin{:});
