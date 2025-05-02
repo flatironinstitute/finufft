f7dac0bd5b3ce08ca44371011d3aad7f8b20990c
Marco Barbone <mbarbone@flatironinstitute.org>
2025-05-02 12:43:57 -0400

    fix-load library

--- python/finufft/finufft/_finufft.py
+++ python/finufft/finufft/_finufft.py
@@ -18,8 +18,6 @@ import numpy as np
 import os
 import platform
 from numpy.ctypeslib import ndpointer
-import logging
-
 
 from packaging.version import Version
 
@@ -37,61 +35,60 @@ if reset_log_level:
     import logging
     log_level = logging.root.level
 
+# TODO: See if there is a way to improve this so it is less hacky.
 lib = None
-log_level = logging.root.level
-reset_log_level = logging.getLogger().getEffectiveLevel() != logging.NOTSET
-
-# Try to load local libfinufft.so next to this file
+# Try to load finufft installed from the python package.
 path = pathlib.Path(__file__).parent.resolve()
-for lib_name in ['libfinufft', 'finufft']:
-    candidate = path / f"{lib_name}.so"
-    if candidate.exists():
-        try:
-            lib = ctypes.CDLL(str(candidate))
-            break
-        except OSError:
-            lib = None
-
-# Try system path as fallback
-if lib is None:
-    libname = find_library('finufft')
-    if libname:
-        try:
-            lib = ctypes.CDLL(libname)
-        except OSError:
-            lib = None
+# Ignoring the exceptions to avoid the print
+# exception, during the process of an exception another exception occurred
+# unix systems have lib prefix, non unix systems do not
+library_names = ['libfinufft', 'finufft']
+for lib_name in library_names:
+    try:
+        lib = np.ctypeslib.load_library(lib_name, path)
+        break
+    except OSError:
+        # Paranoid, in case lib is set to something and then an exception is thrown
+        lib = None
 
 if reset_log_level:
     logging.root.setLevel(log_level)
 
 if lib is None:
-    raise ImportError(
-        'Failed to find a suitable finufft library. '
-        'Please check your installation.'
-    )
+    # If that fails, try to load the library from the system path.
+    libname = find_library('finufft')
+    if libname is not None:
+        lib = ctypes.cdll.LoadLibrary(libname)
+        # we probably should add a version check and trow a warning if the version is different
+    else:
+        # if that does not work, finufft is not installed correctly.
+        raise ImportError('Failed to find a suitable finufft library. '
+                          'Please check your installation, '
+                          'finufft does not seem to be installed correctly.')
+
 
 class FinufftOpts(ctypes.Structure):
     pass
 
 
 FinufftOpts._fields_ = [('modeord', c_int),
-                      ('spreadinterponly', c_int),
-                      ('debug', c_int),
-                      ('spread_debug', c_int),
-                      ('showwarn', c_int),
-                      ('nthreads', c_int),
-                      ('fftw', c_int),
-                      ('spread_sort', c_int),
-                      ('spread_kerevalmeth', c_int),
-                      ('spread_kerpad', c_int),
-                      ('upsampfac', c_double),
-                      ('spread_thread', c_int),
-                      ('maxbatchsize', c_int),
-                      ('spread_nthr_atomic', c_int),
-                      ('spread_max_sp_size', c_int),
-                      ('fftw_lock_fun', c_void_p),
-                      ('fftw_unlock_fun', c_void_p),
-                      ('fftw_lock_data', c_void_p)]
+                        ('spreadinterponly', c_int),
+                        ('debug', c_int),
+                        ('spread_debug', c_int),
+                        ('showwarn', c_int),
+                        ('nthreads', c_int),
+                        ('fftw', c_int),
+                        ('spread_sort', c_int),
+                        ('spread_kerevalmeth', c_int),
+                        ('spread_kerpad', c_int),
+                        ('upsampfac', c_double),
+                        ('spread_thread', c_int),
+                        ('maxbatchsize', c_int),
+                        ('spread_nthr_atomic', c_int),
+                        ('spread_max_sp_size', c_int),
+                        ('fftw_lock_fun', c_void_p),
+                        ('fftw_unlock_fun', c_void_p),
+                        ('fftw_lock_data', c_void_p)]
 
 
 FinufftPlan = c_void_p
