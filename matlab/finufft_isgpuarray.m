2555f0475d8760c987479b25bbae40907829008f
Dan Fortunato <dan.fortunato@gmail.com>
2025-05-08 17:26:37 -0400

    Remove old code

--- matlab/finufft_isgpuarray.m
+++ matlab/finufft_isgpuarray.m
@@ -10,13 +10,4 @@ catch
     is_gpuarray = false;
 end
 
-% % check if the isgpuarray function is available
-% if exist('isgpuarray') == 0
-%   % return 0, since no parallel computing toolbox, can not use gpuarray
-%   is_gpuarray = logical(0);
-% else
-%   % call the parallel computing toolbox isgpuarray function
-%   is_gpuarray = isgpuarray(input);
-% end
-
 end
