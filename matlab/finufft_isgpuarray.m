684e80f9d65cdf66b24a12cba2a5b7bb9a37255d
Dan Fortunato <dan.fortunato@gmail.com>
2025-05-08 16:15:05 -0400

    Speed up gpuArray check in MATLAB interface

--- matlab/finufft_isgpuarray.m
+++ matlab/finufft_isgpuarray.m
@@ -1,15 +1,22 @@
 function is_gpuarray = finufft_isgpuarray(input)
-% FINUFFT_ISGPUARRAY    check if an array is an gpuArray.
-%
-% Note: this is currently unused since GPU codes have distinct cufinufft*
-% names.
+%FINUFFT_ISGPUARRAY   Check if an array is a gpuArray.
 
-% check if the isgpuarray function is available
-    if exist('isgpuarray') == 0
-      % return 0, since no parallel computing toolbox, can not use gpuarray
-      is_gpuarray = logical(0);
-    else
-      % call the parallel computing toolbox isgpuarray function
-      is_gpuarray = isgpuarray(input);
-    end
+try
+    % Try calling MATLAB's built-in isgpuarray routine. If isgpuarray does
+    % not exist (e.g., because we're in Octave) then this will throw an
+    % error, which we catch below.
+    is_gpuarray = isgpuarray(input);
+catch
+    is_gpuarray = false;
+end
+
+% % check if the isgpuarray function is available
+% if exist('isgpuarray') == 0
+%   % return 0, since no parallel computing toolbox, can not use gpuarray
+%   is_gpuarray = logical(0);
+% else
+%   % call the parallel computing toolbox isgpuarray function
+%   is_gpuarray = isgpuarray(input);
+% end
+
 end
