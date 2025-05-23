eeb9d6aeeaf18059cf8ee739833807003cb075c1
Maksim, Eremenko <erjomenko@gmail.com>
2025-05-23 13:37:35 -0400

    Simplified type 3. Fixed syntex typo

--- python/cufinufft/cufinufft/__init__.py
+++ python/cufinufft/cufinufft/__init__.py
@@ -1,7 +1,7 @@
 from cufinufft._plan import Plan
 
 from cufinufft._simple import (nufft1d1, nufft1d2, nufft1d3,
-                               nufft2d1, nufft2d2, nufft2d3
+                               nufft2d1, nufft2d2, nufft2d3,
                                nufft3d1, nufft3d2, nufft3d3)
 
 __all__ = ["nufft1d1", "nufft1d2", "nufft1d3",
