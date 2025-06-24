1a13402efd0ee54efe9bf70a3f694f343ec998a7
Joakim Andén <janden@kth.se>
2025-06-24 22:52:31 +0200

    Fix test_array_ordering test

--- python/cufinufft/tests/test_array_ordering.py
+++ python/cufinufft/tests/test_array_ordering.py
@@ -7,19 +7,17 @@ from cufinufft import Plan, _compat
 import utils
 
 
-def test_type1_ordering(to_gpu, to_cpu, dtype=np.float32, shape=(16, 16, 16), M=4096, tol=1e-3):
-    complex_dtype = utils._complex_dtype(dtype)
-
+def test_type1_ordering(to_gpu, to_cpu, dtype=np.complex64, shape=(16, 16, 16), M=4096, tol=1e-3):
     k, c = utils.type1_problem(dtype, shape, M)
 
     k_gpu = to_gpu(k)
     c_gpu = to_gpu(c)
 
-    plan = Plan(1, shape, eps=tol, dtype=complex_dtype)
+    plan = Plan(1, shape, eps=tol, dtype=dtype)
 
     plan.setpts(*k_gpu)
 
-    out = np.empty(shape, dtype=complex_dtype, order="F")
+    out = np.empty(shape, dtype=dtype, order="F")
 
     out_gpu = to_gpu(out)
 
