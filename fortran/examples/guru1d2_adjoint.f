f74c3022261478fa7f52d27df0d5eead3b796435
ahbarnett <alex.h.barnett@gmail.com>
2025-06-26 12:31:49 -0400

    simplified guru1d2_adjoint{f}.f

--- fortran/examples/guru1d2_adjoint.f
+++ fortran/examples/guru1d2_adjoint.f
@@ -1,22 +1,21 @@
-c     Using guru interface from fortran for 1D type 1 transforms,
-c     a math test of one output, and how to change from default options.
+c     Guru interface from fortran for adjoint of 1D type 2 transform,
+c     (equiv to a type 2 with flipped iflag), math test of one output.
 c     Double-precision only.
 c     Legacy-style: f77, plus dynamic allocation & derived types from f90.
 
 c     To compile (linux/GCC) from this directory, use eg (paste to one line):
 
-c     gfortran -fopenmp -I../../include -I/usr/include guru1d1.f
-c     ../../lib/libfinufft.so -lfftw3 -lfftw3_omp -lgomp -lstdc++ -o guru1d1
+c     gfortran -fopenmp -I../../include -I/usr/include guru1d2_adjoint.f
+c     ../../lib/libfinufft.so -lfftw3 -lfftw3_omp -lgomp -lstdc++
+c     -o guru1d2_adjoint
 
-c     Alex Barnett and Libin Lu 5/29/20. ptr fixes 10/6/21
+c     Martin Reinecke and Alex Barnett, 6/26/25
 
       program guru1d2_adjoint
       implicit none
 
 c     our fortran header, always needed
       include 'finufft.fh'
-c     if you want to use FFTW's modes by name...
-      include 'fftw3.f'
 
 c     note some inputs are int (int*4) but others BIGINT (int*8)
       integer ier,iflag
@@ -33,15 +32,13 @@ c     to pass null pointers to unused arguments...
 
 c     this is what you use as the "opaque" ptr to ptr to finufft_plan...
       integer*8 plan
-c     this is how you create the options struct in fortran...
-      type(finufft_opts) opts
 c     or this is if you want default opts, make a null pointer...
       type(finufft_opts), pointer :: defopts => null()
 
 
 c     how many nonuniform pts
       M = 1000000
-c     how many modes (not too much since FFTW_MEASURE slow later)
+c     how many modes
       N = 100000
 
       allocate(fk(N))
@@ -55,8 +52,7 @@ c     create some quasi-random NU pts in [-pi, pi), complex strengths
          cj(j) = dcmplx( dsin((100d0*j)/M), dcos(1.0+(50d0*j)/M))
       enddo
 
-c     ---- SIMPLEST GURU DEMO WITH DEFAULT OPTS (as in simple1d1) ---------
-c     mandatory parameters to FINUFFT guru interface... (ttype = trans type)
+c     mandatory parameters to FINUFFT guru interface...
       ttype = 2
       dim = 1
       ntrans = 1
@@ -72,7 +68,8 @@ c     use default options
 c     note for ttype 1 or 2, arguments 6-9 ignored...
       call finufft_setpts(plan,M,xj,dummy,dummy,dummy,
      $     dummy,dummy,dummy,ier)
-c     Do it: reads cj (strengths), writes fk (mode coeffs) and ier (status)
+c     Do the adjoint of planned transform:
+c     reads fk (mode coeffs), writes cj (strengths) and ier (status)
       call finufft_execute_adjoint(plan,cj,fk,ier)
       call system_clock(t2,crate)
       t = (t2-t1)/float(crate)
@@ -88,6 +85,7 @@ c     math test: single output mode with given freq (not array index) k
       ktest = N/3
       fktest = dcmplx(0,0)
       do j=1,M
+c     note flipped iflag on the sin term, since adjoint...
          fktest = fktest + cj(j) * dcmplx( dcos(ktest*xj(j)),
      $        dsin(-iflag*ktest*xj(j)) )
       enddo
@@ -99,40 +97,6 @@ c     compute inf norm of fk coeffs for use in rel err
       ktestindex = ktest + N/2 + 1
       print '("rel err for mode k=",i10," is ",e10.2)',ktest,
      $     cdabs(fk(ktestindex)-fktest)/fmax
-
-c     ----------- GURU DEMO WITH NEW OPTIONS, MULTIPLE EXECS ----------
-      print *,''
-      print *, 'setting new options, rerun guru interface...'
-      call finufft_default_opts(opts)
-c     refer to fftw3.f to set various FFTW plan modes...
-      opts%fftw = FFTW_ESTIMATE_PATIENT
-      opts%debug = 1
-c     note you need a fresh plan if change opts
-      call finufft_makeplan(ttype,dim,n_modes,iflag,ntrans,
-     $     tol,plan,opts,ier)
-      call finufft_setpts(plan,M,xj,dummy,dummy,dummy,
-     $     dummy,dummy,dummy,ier)
-c     Do it: reads cj (strengths), writes fk (mode coeffs) and ier (status)
-      call finufft_execute_adjoint(plan,cj,fk,ier)
-c     change the strengths
-      do j = 1,M
-         cj(j) = dcmplx( dsin((10d0*j)/M), dcos(2.0+(20d0*j)/M))
-      enddo
-c     do another transform using same NU pts
-      call finufft_execute_adjoint(plan,cj,fk,ier)
-c     change the NU pts then do another transform w/ existing strengths...
-      do j = 1,M
-         xj(j) = pi/2.0 * dcos(pi*j/M)
-      enddo
-      call finufft_setpts(plan,M,xj,dummy,dummy,dummy,
-     $     dummy,dummy,dummy,ier)
-      call finufft_execute_adjoint(plan,cj,fk,ier)
-      if (ier.eq.0) then
-         print *,'done.'
-      else
-         print *,'failed! ier=',ier
-      endif
-      call finufft_destroy(plan,ier)
 
       stop
       end
--- fortran/examples/guru1d2_adjointf.f
+++ fortran/examples/guru1d2_adjointf.f
@@ -1,22 +1,21 @@
-c     Using guru interface from fortran for 1D type 1 transforms,
-c     a math test of one output, and how to change from default options.
+c     Guru interface from fortran for adjoint of 1D type 2 transform,
+c     (equiv to a type 2 with flipped iflag), math test of one output.
 c     Single-precision only.
 c     Legacy-style: f77, plus dynamic allocation & derived types from f90.
 
 c     To compile (linux/GCC) from this directory, use eg (paste to one line):
 
-c     gfortran-9 -fopenmp -I../../include -I/usr/include guru1d1f.f
-c     -L../../lib -lfinufftf -o guru1d1f
+c     gfortran -fopenmp -I../../include -I/usr/include guru1d2_adjointf.f
+c     ../../lib/libfinufft.so -lfftw3 -lfftw3_omp -lgomp -lstdc++
+c     -o guru1d2_adjointf
 
-c     Alex Barnett and Libin Lu 5/29/20. Ptr fixes 10/6/21
+c     Martin Reinecke and Alex Barnett, 6/26/25
 
-      program guru1d1f
+      program guru1d2_adjointf
       implicit none
 
-c     our fortran-header, always needed
+c     our fortran header, always needed
       include 'finufft.fh'
-c     if you want to use FFTW's modes by name...
-      include 'fftw3.f'
 
 c     note some inputs are int (int*4) but others BIGINT (int*8)
       integer ier,iflag
@@ -33,15 +32,14 @@ c     to pass null pointers to unused arguments...
 
 c     this is what you use as the "opaque" ptr to ptr to finufft_plan...
       integer*8 plan
-c     this is how you create the options struct in fortran...
-      type(finufft_opts) opts
 c     or this is if you want default opts, make a null pointer...
       type(finufft_opts), pointer :: defopts => null()
 
+
 c     how many nonuniform pts
-      M = 200000
-c     how many modes (not too much since FFTW_MEASURE slow later)
-      N = 100000
+      M = 1000000
+c     how many modes
+      N = 30000
 
       allocate(fk(N))
       allocate(xj(M))
@@ -51,11 +49,10 @@ c     how many modes (not too much since FFTW_MEASURE slow later)
 c     create some quasi-random NU pts in [-pi, pi), complex strengths
       do j = 1,M
          xj(j) = pi * cos(pi*j/M)
-         cj(j) = cmplx( sin((100e0*j)/M), cos(1.0+(50e0*j)/M))
+         cj(j) = dcmplx( sin((100e0*j)/M), cos(1.0+(50e0*j)/M))
       enddo
 
-c     ---- SIMPLEST GURU DEMO WITH DEFAULT OPTS (as in simple1d1) ---------
-c     mandatory parameters to FINUFFT guru interface... (ttype = trans type)
+c     mandatory parameters to FINUFFT guru interface...
       ttype = 2
       dim = 1
       ntrans = 1
@@ -68,10 +65,11 @@ c     (note since dim=1, unused entries on n_modes are never read)
 c     use default options
       call finufftf_makeplan(ttype,dim,n_modes,iflag,ntrans,
      $     tol,plan,defopts,ier)
-c     note for type 1 or 2, arguments 6-9 ignored...
+c     note for ttype 1 or 2, arguments 6-9 ignored...
       call finufftf_setpts(plan,M,xj,dummy,dummy,dummy,
      $     dummy,dummy,dummy,ier)
-c     Do it: reads cj (strengths), writes fk (mode coeffs) and ier (status)
+c     Do the adjoint of planned transform:
+c     reads fk (mode coeffs), writes cj (strengths) and ier (status)
       call finufftf_execute_adjoint(plan,cj,fk,ier)
       call system_clock(t2,crate)
       t = (t2-t1)/float(crate)
@@ -87,6 +85,7 @@ c     math test: single output mode with given freq (not array index) k
       ktest = N/3
       fktest = cmplx(0,0)
       do j=1,M
+c     note flipped iflag on the sin term, since adjoint...
          fktest = fktest + cj(j) * cmplx( cos(ktest*xj(j)),
      $        sin(-iflag*ktest*xj(j)) )
       enddo
@@ -98,40 +97,6 @@ c     compute inf norm of fk coeffs for use in rel err
       ktestindex = ktest + N/2 + 1
       print '("rel err for mode k=",i10," is ",e10.2)',ktest,
      $     cabs(fk(ktestindex)-fktest)/fmax
-
-c     ----------- GURU DEMO WITH NEW OPTIONS, MULTIPLE EXECS ----------
-      print *,''
-      print *, 'setting new options, rerun guru interface...'
-      call finufftf_default_opts(opts)
-c     refer to fftw3.f to set various FFTW plan modes...
-      opts%fftw = FFTW_ESTIMATE_PATIENT
-      opts%debug = 1
-c     note you need a fresh plan if change opts
-      call finufftf_makeplan(ttype,dim,n_modes,iflag,ntrans,
-     $     tol,plan,opts,ier)
-      call finufftf_setpts(plan,M,xj,dummy,dummy,dummy,
-     $     dummy,dummy,dummy,ier)
-c     Do it: reads cj (strengths), writes fk (mode coeffs) and ier (status)
-      call finufftf_execute_adjoint(plan,cj,fk,ier)
-c     change the strengths
-      do j = 1,M
-         cj(j) = cmplx( sin((10e0*j)/M), cos(2.0+(20e0*j)/M))
-      enddo
-c     do another transform using same NU pts
-      call finufftf_execute_adjoint(plan,cj,fk,ier)
-c     change the NU pts then do another transform w/ existing strengths...
-      do j = 1,M
-         xj(j) = pi/2.0 * cos(pi*j/M)
-      enddo
-      call finufftf_setpts(plan,M,xj,dummy,dummy,dummy,
-     $     dummy,dummy,dummy,ier)
-      call finufftf_execute_adjoint(plan,cj,fk,ier)
-      if (ier.eq.0) then
-         print *,'done.'
-      else
-         print *,'failed! ier=',ier
-      endif
-      call finufftf_destroy(plan,ier)
 
       stop
       end
