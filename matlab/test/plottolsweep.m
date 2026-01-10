08b16bc2c15656b6b7fb677a3a6f2c4d57409466
ahbarnett <alex.h.barnett@gmail.com>
2026-01-10 16:21:53 -0500

    playing with plottolsweep

--- matlab/test/plottolsweep.m
+++ matlab/test/plottolsweep.m
@@ -17,7 +17,7 @@ ntr = 10;             % #transforms to average error over
 isign = +1;
 o.debug = 0; o.showwarn=0;        % any FINUFFT opts...
 o.upsampfac = 1.25;  %2.0  etc
-o.spread_kerformula = 1;
+o.spread_kerformula = 0;
 % failure fudge factors...
 tolslack = [5.0; 5.0; 10.0];  % factors by which eps can exceed tol (3 types)
 floorslack = 20;
