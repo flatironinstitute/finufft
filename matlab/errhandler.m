fcd2f94bbdc871c212e620df60e3d87bebdb68f4
ahbarnett <alex.h.barnett@gmail.com>
2026-01-29 19:38:23 -0500

    add errhandler.m codes 13-24, working but not fixing 797. [no ci]

--- matlab/errhandler.m
+++ matlab/errhandler.m
@@ -1,12 +1,14 @@
 function errhandler(ier)
 % ERRHANDLER  translate FINUFFT's ier status into MATLAB warnings/error throws.
 
-% Barnett 6/13/20
+% Barnett 6/13/20, updated 1/29/26.
 
 % Note that there are other matlab-only error types defined in valid_*.m
 
 switch ier
- % These are the ERR_ #defines in ../include/finufft_errors.h:
+  % These should match the ERR_ #defines in ../include/finufft_errors.h
+  % All of those should be caught here, else a C++ error will crash MATLAB entirely
+  % and make for unhappy users:
  case 1
   warning('FINUFFT:epsTooSmall','FINUFFT eps tolerance too small to achieve');
  case 2
@@ -22,7 +24,7 @@ switch ier
  case 7
   error('FINUFFT:upsampfacSmall','FINUFFT opts.upsampfac not > 1.0');
  case 8
-  error('FINUFFT:upsampfacNotHorner','FINUFFT opts.upsampfac not a value with known Horner polynomial rule');
+  error('FINUFFT:GPU:upsampfacNotHorner','FINUFFT GPU unsupported sigma for Horner evaluation method');
  case 9
   error('FINUFFT:badNtrans','FINUFFT number of transforms ntrans invalid');
  case 10
@@ -31,4 +33,28 @@ switch ier
   error('FINUFFT:malloc','FINUFFT general malloc failure');
  case 12
   error('FINUFFT:badDim','FINUFFT number of dimensions dim invalid');
+ case 13
+  error('FINUFFT:spreadinterp:badNthr','FINUFFT spread threads invalid');
+ case 14
+  error('FINUFFT:GPU:badDim','FINUFFT GPU number of dimensions dim invalid');
+ case 15
+  error('FINUFFT:GPU:failure','FINUFFT GPU general failure');
+ case 16
+  error('FINUFFT:GPU:badPlan','FINUFFT GPU plan not valid');
+ case 17
+  error('FINUFFT:GPU:badMethod','FINUFFT GPU method not valid');
+ case 18
+  error('FINUFFT:GPU:badBinSize','FINUFFT GPU internal bin size not valid');
+ case 19
+  error('FINUFFT:GPU:mallocShMem','FINUFFT GPU insufficient shared memory');
+ case 20
+  error('FINUFFT:badNNU','FINUFFT invalid number of nonuniform points (M or N)');
+ case 21
+  error('FINUFFT:GPU:badArg','FINUFFT GPU invalid argument (eg to setpts)');
+ case 22
+  error('FINUFFT:badLockFun','FINUFFT invalid lock functions for FFTW');
+ case 23
+  error('FINUFFT:badNthr','FINUFFT number of threads invalid');
+ case 24
+  error('FINUFFT:badKerFormula','FINUFFT opts.spread_kerformula invalid');
 end
