cf55aa1a1ab467c334b52cfabedfce67a22d8802
Marco Barbone <mbarbone@flatironinstitute.org>
2026-02-24 16:04:08 -0500

    Move spread/interp free functions to FINUFFT_PLAN_T; consolidate FFT plan into fft.cpp
    - Move Finufft_FFT_plan to fft.hpp; convert do_fft/gridsize_for_fft to methods
    - Move spread/interp free functions into FINUFFT_PLAN_T as private methods
    - Merge spread/interp wrapper TUs; fix namespace pollution in headers
    - Revert NDIMS dispatch in interpSorted; add change notes to moved functions
    - Move Finufft_FFT_plan definition to src/fft.cpp (opaque to other TUs)

--- src/setpts.cpp
+++ src/setpts.cpp
@@ -8,6 +8,10 @@ using FLT = float;
 using FLT = double;
 #endif
 
+extern template void FINUFFT_PLAN_T<FLT>::indexSort();
+extern template FLT FINUFFT_PLAN_T<FLT>::evaluate_kernel_runtime(FLT) const;
+extern template int FINUFFT_PLAN_T<FLT>::init_grid_kerFT_FFT(); // instantiated in fft.cpp
+
 template int FINUFFT_PLAN_T<FLT>::setpts(BIGINT nj, const FLT *xj, const FLT *yj,
                                          const FLT *zj, BIGINT nk, const FLT *s,
                                          const FLT *t, const FLT *u);
