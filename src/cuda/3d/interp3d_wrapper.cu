f4b292afa37b483d224b1c8669dd15996f4f75a0
Martin Reinecke <martin@mpa-garching.mpg.de>
2026-03-04 13:39:04 +0100

    experiment

--- src/cuda/3d/interp3d_wrapper.cu
+++ src/cuda/3d/interp3d_wrapper.cu
@@ -17,63 +17,45 @@ namespace spreadinterp {
 
 template<typename T, int KEREVALMETH, int ns>
 static __global__ void interp_3d_nupts_driven(
-    const T *x, const T *y, const T *z, cuda_complex<T> *c, const cuda_complex<T> *fw,
-    int M, int nf1, int nf2, int nf3, T es_c, T es_beta, T sigma, const int *idxnupts) {
+    const cuda::std::array<const T *, 3> xyz, cuda_complex<T> *c,
+    const cuda_complex<T> *fw, int M, const cuda::std::array<int, 3> nf, T es_c,
+    T es_beta, T sigma, const int *idxnupts) {
 
-  T ker1[ns];
-  T ker2[ns];
-  T ker3[ns];
+  cuda::std::array<cuda::std::array<T, ns>, 3> ker;
 
   for (int i = blockDim.x * blockIdx.x + threadIdx.x; i < M;
        i += blockDim.x * gridDim.x) {
-    const auto nuptsidx   = loadReadOnly(idxnupts + i);
-    const auto x_rescaled = fold_rescale(loadReadOnly(x + nuptsidx), nf1);
-    const auto y_rescaled = fold_rescale(loadReadOnly(y + nuptsidx), nf2);
-    const auto z_rescaled = fold_rescale(loadReadOnly(z + nuptsidx), nf3);
+    const auto nuptsidx = loadReadOnly(idxnupts + i);
 
-    auto [xstart, xend] = interval(ns, x_rescaled);
-    auto [ystart, yend] = interval(ns, y_rescaled);
-    auto [zstart, zend] = interval(ns, z_rescaled);
-
-    const T x1 = T(xstart) - x_rescaled;
-    const T y1 = T(ystart) - y_rescaled;
-    const T z1 = T(zstart) - z_rescaled;
-
-    if constexpr (KEREVALMETH == 1) {
-      eval_kernel_vec_horner<T, ns>(ker1, x1, sigma);
-      eval_kernel_vec_horner<T, ns>(ker2, y1, sigma);
-      eval_kernel_vec_horner<T, ns>(ker3, z1, sigma);
-    } else {
-      eval_kernel_vec<T, ns>(ker1, x1, es_c, es_beta);
-      eval_kernel_vec<T, ns>(ker2, y1, es_c, es_beta);
-      eval_kernel_vec<T, ns>(ker3, z1, es_c, es_beta);
+    cuda::std::array<int, 3> start;
+    for (size_t idim = 0; idim < 3; ++idim) {
+      auto rescaled   = fold_rescale(loadReadOnly(xyz[idim] + nuptsidx), nf[idim]);
+      auto [s, dummy] = interval(ns, rescaled);
+      if constexpr (KEREVALMETH == 1) {
+        eval_kernel_vec_horner<T, ns>(&ker[idim][0], T(s) - rescaled, sigma);
+      } else {
+        eval_kernel_vec<T, ns>(&ker[idim][0], T(s) - rescaled, es_c, es_beta);
+      }
+      start[idim] = s + (s < 0) ? nf[idim] : 0;
     }
 
-    const int offset1 = xstart < 0 ? nf1 : 0;
-    xstart += offset1;
-    xend += offset1;
-    const int offset2 = ystart < 0 ? nf2 : 0;
-    ystart += offset2;
-    yend += offset2;
-    const int offset3 = zstart < 0 ? nf3 : 0;
-    zstart += offset3;
-    zend += offset3;
-
-    cuda_complex<T> cnow{0, 0};
-    int xidx[ns];
-    for (int x0 = 0, ix = xstart; x0 < ns; ++x0, ++ix, ix = (ix >= nf1) ? ix - nf1 : ix)
+    cuda::std::array<int, ns> xidx;
+    for (int x0 = 0, ix = start[0]; x0 < ns;
+         ++x0, ++ix, ix = (ix >= nf[0]) ? ix - nf[0] : ix)
       xidx[x0] = ix;
-    for (int z0 = 0, iz = zstart; z0 < ns; ++z0, ++iz, iz = (iz >= nf3) ? iz - nf3 : iz) {
-      const auto inidx0 = iz * nf2 * nf1;
+    cuda_complex<T> cnow{0, 0};
+    for (int z0 = 0, iz = start[2]; z0 < ns;
+         ++z0, ++iz, iz = (iz >= nf[2]) ? iz - nf[2] : iz) {
+      const auto inidx0 = iz * nf[1] * nf[0];
       cuda_complex<T> cnowy{0, 0};
-      for (int y0 = 0, iy = ystart; y0 < ns;
-           ++y0, ++iy, iy = (iy >= nf2) ? iy - nf2 : iy) {
-        const auto inidx1 = inidx0 + iy * nf1;
+      for (int y0 = 0, iy = start[1]; y0 < ns;
+           ++y0, ++iy, iy = (iy >= nf[1]) ? iy - nf[1] : iy) {
+        const auto inidx1 = inidx0 + iy * nf[0];
         cuda_complex<T> cnowx{0, 0};
-        for (int x0 = 0; x0 < ns; ++x0) cnowx += fw[inidx1 + xidx[x0]] * ker1[x0];
-        cnowy += cnowx * ker2[y0];
+        for (int x0 = 0; x0 < ns; ++x0) cnowx += fw[inidx1 + xidx[x0]] * ker[0][x0];
+        cnowy += cnowx * ker[1][y0];
       }
-      cnow += cnowy * ker3[z0];
+      cnow += cnowy * ker[2][z0];
     }
     c[idxnupts[i]] = cnow;
   }
@@ -193,11 +175,7 @@ static void cuinterp3d_nuptsdriven(int nf1, int nf2, int nf3, int M,
   T sigma   = d_plan.spopts.upsampfac;
 
   const int *d_idxnupts = dethrust(d_plan.idxnupts);
-  ;
 
-  const T *d_kx               = d_plan.kxyz[0];
-  const T *d_ky               = d_plan.kxyz[1];
-  const T *d_kz               = d_plan.kxyz[2];
   cuda_complex<T> *d_c        = d_plan.c;
   const cuda_complex<T> *d_fw = d_plan.fw;
 
@@ -208,15 +186,15 @@ static void cuinterp3d_nuptsdriven(int nf1, int nf2, int nf3, int M,
   if (d_plan.opts.gpu_kerevalmeth) {
     for (int t = 0; t < blksize; t++) {
       interp_3d_nupts_driven<T, 1, ns><<<blocks, threadsPerBlock, 0, stream>>>(
-          d_kx, d_ky, d_kz, d_c + t * M, d_fw + t * nf1 * nf2 * nf3, M, nf1, nf2, nf3,
-          es_c, es_beta, sigma, d_idxnupts);
+          d_plan.kxyz, d_c + t * M, d_fw + t * d_plan.nf, M, d_plan.nf123, es_c, es_beta,
+          sigma, d_idxnupts);
       THROW_IF_CUDA_ERROR
     }
   } else {
     for (int t = 0; t < blksize; t++) {
       interp_3d_nupts_driven<T, 0, ns><<<blocks, threadsPerBlock, 0, stream>>>(
-          d_kx, d_ky, d_kz, d_c + t * M, d_fw + t * nf1 * nf2 * nf3, M, nf1, nf2, nf3,
-          es_c, es_beta, sigma, d_idxnupts);
+          d_plan.kxyz, d_c + t * M, d_fw + t * d_plan.nf, M, d_plan.nf123, es_c, es_beta,
+          sigma, d_idxnupts);
       THROW_IF_CUDA_ERROR
     }
   }
