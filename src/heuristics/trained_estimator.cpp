776cbca613f4b81896d1eb4bf4af4c367fde30a9
Artur <magisterbrownie@gmail.com>
2026-03-25 17:18:19 +0100

    Heuristics predictors

---
+++ src/heuristics/trained_estimator.cpp
@@ -0,0 +1,4 @@
+#include <finufft/heuristics.hpp>
+const std::vector<finufft::heuristics::SigmaEstimator> finufft::heuristics::trained = {
+finufft::heuristics::SigmaEstimator(1,1,{1.04249,0.0932473,0.287557,-0.473353},0.000203721,0.00503031,typeid(float))
+};
