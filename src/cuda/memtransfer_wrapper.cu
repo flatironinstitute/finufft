a436c283a797bd59d818009d0df1b37f35cdc85a
Martin Reinecke <martin@mpa-garching.mpg.de>
2026-03-26 12:23:34 +0100

    cleanup

--- src/cuda/memtransfer_wrapper.cu
+++ src/cuda/memtransfer_wrapper.cu
@@ -8,131 +8,42 @@
 #include <cufinufft/contrib/helper_cuda.h>
 
 template<typename T>
-void cufinufft_plan_t<T>::alloc1d()
-/*
-    wrapper for gpu memory allocation in "plan" stage.
-
-    Melody Shih 11/21/21
-*/
-{
-  int nf1 = nf123[0];
+void cufinufft_plan_t<T>::allocate() {
+  cuda::std::array<int, 3> binsizes {opts.gpu_binsizex, opts.gpu_binsizey, opts.gpu_binsizez};
 
   switch (opts.gpu_method) {
   case 1: {
     if (opts.gpu_sort) {
-      int numbins = ceil((T)nf1 / opts.gpu_binsizex);
+      int numbins = 1;
+      for (int idim=0; idim<dim; ++idim)
+        numbins *= ceil((T)nf123[idim] / binsizes[idim]);
       binsize.resize(numbins);
       binstartpts.resize(numbins);
     }
   } break;
   case 2:
   case 3: {
-    int numbins = ceil((T)nf1 / opts.gpu_binsizex);
+    int numbins = 1;
+    for (int idim=0; idim<dim; ++idim)
+      numbins *= ceil((T)nf123[idim] / binsizes[idim]);
     numsubprob.resize(numbins);
     binsize.resize(numbins);
     binstartpts.resize(numbins);
     subprobstartpts.resize(numbins + 1);
   } break;
-  default:
-    std::cerr << "err: invalid method " << std::endl;
-    throw int(FINUFFT_ERR_METHOD_NOTVALID);
-  }
-
-  if (!opts.gpu_spreadinterponly) {
-    fwkerhalf[0].resize(nf1 / 2 + 1);
-  }
-}
-
-template<typename T>
-void cufinufft_plan_t<T>::alloc2d()
-/*
-    wrapper for gpu memory allocation in "plan" stage.
-
-    Melody Shih 07/25/19
-*/
-{
-  int nf1 = nf123[0];
-  int nf2 = nf123[1];
-
-  switch (opts.gpu_method) {
-  case 1: {
-    if (opts.gpu_sort) {
-      int numbins[2];
-      numbins[0] = ceil((T)nf1 / opts.gpu_binsizex);
-      numbins[1] = ceil((T)nf2 / opts.gpu_binsizey);
-      binsize.resize(numbins[0] * numbins[1]);
-      binstartpts.resize(numbins[0] * numbins[1]);
-    }
-  } break;
-  case 2:
-  case 3: {
-    int64_t numbins[2];
-    numbins[0] = ceil((T)nf1 / opts.gpu_binsizex);
-    numbins[1] = ceil((T)nf2 / opts.gpu_binsizey);
-    numsubprob.resize(numbins[0] * numbins[1]);
-    binsize.resize(numbins[0] * numbins[1]);
-    binstartpts.resize(numbins[0] * numbins[1]);
-    subprobstartpts.resize(numbins[0] * numbins[1] + 1);
-  } break;
-  default:
-    std::cerr << "[allocgpumem2d_plan] error: invalid method\n";
-    throw int(FINUFFT_ERR_METHOD_NOTVALID);
-  }
-
-  if (!opts.gpu_spreadinterponly) {
-    fwkerhalf[0].resize(nf1 / 2 + 1);
-    fwkerhalf[1].resize(nf2 / 2 + 1);
-  }
-}
-
-template<typename T>
-void cufinufft_plan_t<T>::alloc3d()
-/*
-    wrapper for gpu memory allocation in "plan" stage.
-
-    Melody Shih 07/25/19
-*/
-{
-  int nf1 = nf123[0];
-  int nf2 = nf123[1];
-  int nf3 = nf123[2];
-
-  switch (opts.gpu_method) {
-  case 1: {
-    if (opts.gpu_sort) {
-      const int64_t nbins_tot = ceil((T)nf1 / opts.gpu_binsizex) *
-                                ceil((T)nf2 / opts.gpu_binsizey) *
-                                ceil((T)nf3 / opts.gpu_binsizez);
-      binsize.resize(nbins_tot);
-      binstartpts.resize(nbins_tot);
-    }
-  } break;
-  case 2:
-  case 3: {
-    const int64_t nbins_tot = ceil((T)nf1 / opts.gpu_binsizex) *
-                              ceil((T)nf2 / opts.gpu_binsizey) *
-                              ceil((T)nf3 / opts.gpu_binsizez);
-
-    numsubprob.resize(nbins_tot);
-    binsize.resize(nbins_tot);
-    binstartpts.resize(nbins_tot);
-    subprobstartpts.resize(nbins_tot + 1);
-  } break;
   case 4: {
-    const int numobins[3] = {(int)ceil((T)nf1 / opts.gpu_obinsizex),
-                             (int)ceil((T)nf2 / opts.gpu_obinsizey),
-                             (int)ceil((T)nf3 / opts.gpu_obinsizez)};
-
-    const int binsperobins[3] = {opts.gpu_obinsizex / opts.gpu_binsizex,
-                                 opts.gpu_obinsizey / opts.gpu_binsizey,
-                                 opts.gpu_obinsizez / opts.gpu_binsizez};
-
-    const int numbins[3] = {numobins[0] * (binsperobins[0] + 2),
-                            numobins[1] * (binsperobins[1] + 2),
-                            numobins[2] * (binsperobins[2] + 2)};
-
-    const int64_t numobins_tot = numobins[0] * numobins[1] * numobins[2];
-    const int64_t numbins_tot  = numbins[0] * numbins[1] * numbins[2];
+    if (dim!=3) {
+      std::cerr << "err: invalid method " << std::endl;
+      throw int(FINUFFT_ERR_METHOD_NOTVALID);
+    }
+    cuda::std::array<int, 3> obinsizes {opts.gpu_obinsizex, opts.gpu_obinsizey, opts.gpu_obinsizez};
+    int numobins_tot=1, numbins_tot=1;
+    for (int idim=0; idim<dim; ++idim) {
+      const int numobins = (int)ceil((T)nf123[idim] / obinsizes[idim]);
+      numobins_tot *= numobins;
+      const int binsperobin = obinsizes[idim] / binsizes[idim];
+      numbins_tot *= numobins * (binsperobin + 2);
+    }
 
     numsubprob.resize(numobins_tot);
     binsize.resize(numbins_tot);
@@ -143,19 +54,11 @@ void cufinufft_plan_t<T>::alloc3d()
     std::cerr << "[allocgpumem3d_plan] error: invalid method\n";
     throw int(FINUFFT_ERR_METHOD_NOTVALID);
   }
-
-  if (!opts.gpu_spreadinterponly) {
-    fwkerhalf[0].resize(nf1 / 2 + 1);
-    fwkerhalf[1].resize(nf2 / 2 + 1);
-    fwkerhalf[2].resize(nf3 / 2 + 1);
-  }
+  if (!opts.gpu_spreadinterponly)
+    for (int idim=0; idim<dim; ++idim)
+      fwkerhalf[idim].resize(nf123[idim] / 2 + 1);
 }
 
-template<typename T> void cufinufft_plan_t<T>::allocate() {
-  if (dim == 1) alloc1d();
-  if (dim == 2) alloc2d();
-  if (dim == 3) alloc3d();
-}
 template void cufinufft_plan_t<float>::allocate();
 template void cufinufft_plan_t<double>::allocate();
 
